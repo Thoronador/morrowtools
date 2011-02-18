35d4610f84187e310b96ae4d2b7b3485afec17ff
thoronador <thoronador@users.sourceforge.net>
2011-02-18 02:37:28 +0000

    Record structure and manager class for start scripts added.

---
+++ base/StartScripts.h
@@ -0,0 +1,107 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of the Morrowind Tools Project.
+    Copyright (C) 2011 Thoronador
+
+    The Morrowind Tools are free software: you can redistribute them and/or
+    modify them under the terms of the GNU General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The Morrowind Tools are distributed in the hope that they will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with the Morrowind Tools.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#ifndef STARTSCRIPTS_H
+#define STARTSCRIPTS_H
+
+#include <string>
+#include <map>
+#include "records/StartScriptRecord.h"
+
+//iterator type for StartScript list
+typedef std::map<std::string, StartScriptRecord>::const_iterator StartScriptListIterator;
+
+class StartScripts
+{
+  public:
+    /* destructor */
+    ~StartScripts();
+
+    /* singleton access method */
+    static StartScripts& getSingleton();
+
+    /* adds a StartScript to the list */
+    void addStartScript(const StartScriptRecord& record);
+
+    /* returns true, if a StartScript with the given ID is present
+
+       parameters:
+           ID - the ID of the StartScript object
+    */
+    bool hasStartScript(const std::string& ID) const;
+
+    /* returns the number of StartScripts in the list */
+    unsigned int getNumberOfStartScripts() const;
+
+    /* returns a reference to the StartScript record of the StartScript with the
+       given ID
+
+       parameters:
+           ID - the ID of the StartScript
+
+       remarks:
+           If no StartScript with the given ID is present, the function will throw
+           an exception. Use hasStartScript() to determine, if a StartScript with the
+           desired ID is present.
+    */
+    const StartScriptRecord& getStartScript(const std::string& ID) const;
+
+    /* tries to read a StartScript record from the given input file stream.
+
+       return value:
+           If an error occured, the function returns -1. Otherwise it returns
+           the number of updated records. (Usually that is one. If, however, the
+           record that was read is equal to the one already in the list, zero is
+           returned.)
+
+       parameters:
+           in_File  - the input file stream that is used to read the record
+    */
+    int readRecordSSCR(std::ifstream& in_File);
+
+    /* returns constant iterator to the beginning of the internal list */
+    StartScriptListIterator getBegin() const;
+
+    /* returns constant iterator to the end of the internal list */
+    StartScriptListIterator getEnd() const;
+
+    /* tries to save all available StartScripts to the given stream and returns
+       true on success, false on failure
+
+       parameters:
+           output - the output file stream that shall be used to save the
+                    StartScripts
+    */
+    bool saveAllToStream(std::ofstream& output) const;
+
+    /* removes all StartScripts from the list */
+    void clearAll();
+  private:
+    /* constructor */
+    StartScripts();
+
+    /* empty copy constructor */
+    StartScripts(const StartScripts& op) {}
+
+    /* internal data */
+    std::map<std::string, StartScriptRecord> m_StartScripts;
+};//class
+
+#endif // STARTSCRIPTS_H
---
+++ base/records/StartScriptRecord.cpp
@@ -0,0 +1,147 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of the Morrowind Tools Project.
+    Copyright (C) 2009, 2011 Thoronador
+
+    The Morrowind Tools are free software: you can redistribute them and/or
+    modify them under the terms of the GNU General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The Morrowind Tools are distributed in the hope that they will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with the Morrowind Tools.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#include "StartScriptRecord.h"
+#include <iostream>
+#include "../MW_Constants.h"
+#include "../HelperIO.h"
+
+StartScriptRecord::StartScriptRecord()
+{
+  Data = "";
+  Name = "";
+}
+
+StartScriptRecord::StartScriptRecord(const std::string& ID)
+{
+  Data = "";
+  Name = ID;
+}
+
+bool StartScriptRecord::equals(const StartScriptRecord& other) const
+{
+  return ((Data==other.Data) and (Name==other.Name));
+}
+
+bool StartScriptRecord::saveToStream(std::ofstream& output) const
+{
+  output.write((char*) &cSSCR, 4);
+  int32_t Size, HeaderOne, H_Flags;
+  HeaderOne = H_Flags = 0;
+  Size = 4 /* DATA */ +4 /* 4 bytes for length */
+        +Data.length()+1 /* length of sequence (no NUL termination) */
+        +4 /* NAME */ +4 /* 4 bytes for length */
+        +Name.length() /* length of name (no NUL termination) */;
+  output.write((char*) &Size, 4);
+  #warning HeaderOne and H_Flags might not be initialized properly!
+  output.write((char*) &HeaderOne, 4);
+  output.write((char*) &H_Flags, 4);
+
+  /*Start Script(?): (no documentation known)
+    DATA = ? (a sequence of digits)
+    NAME = ID string*/
+
+  //write DATA
+  output.write((char*) &cDATA, 4);
+  int32_t SubLength = Data.length();
+  //write DATA's length
+  output.write((char*) &SubLength, 4);
+  //write data
+  output.write(Data.c_str(), SubLength);
+
+  //write NAME
+  output.write((char*) &cNAME, 4);
+  SubLength = Name.length();
+  //write NAME's length
+  output.write((char*) &SubLength, 4);
+  //write Name
+  output.write(Name.c_str(), SubLength);
+  return output.good();
+}
+
+bool StartScriptRecord::loadFromStream(std::ifstream& in_File)
+{
+  int32_t Size, HeaderOne, H_Flags;
+  in_File.read((char*) &Size, 4);
+  in_File.read((char*) &HeaderOne, 4);
+  in_File.read((char*) &H_Flags, 4);
+
+  /*Start Script(?): (no documentation known)
+    DATA = ? (a sequence of digits)
+    NAME = ID string */
+  int32_t SubRecName, SubLength;
+  SubRecName = SubLength = 0;
+
+  //read DATA
+  in_File.read((char*) &SubRecName, 4);
+  if (SubRecName!=cDATA)
+  {
+    UnexpectedRecord(cDATA, SubRecName);
+    return false;
+  }
+  //DATA's length
+  in_File.read((char*) &SubLength, 4);
+  if (SubLength>255)
+  {
+    std::cout << "StartScriptRecord::loadFromStream: Error: data sequence is "
+              << "longer than 255 characters.\nFile position: "<<in_File.tellg()
+              <<" bytes.\n";
+    return false;
+  }
+  //read data
+  char Buffer[256];
+  memset(Buffer, '\0', 256);
+  in_File.read(Buffer, SubLength);
+  if (!in_File.good())
+  {
+    std::cout << "StartScriptRecord::loadFromStream: Error while reading data sequence from stream.\n";
+    std::cout << "File position: "<<in_File.tellg()<<" bytes\n";
+    return false;
+  }
+  Data = std::string(Buffer);
+
+  //read NAME
+  in_File.read((char*) &SubRecName, 4);
+  if (SubRecName!=cNAME)
+  {
+    UnexpectedRecord(cNAME, SubRecName);
+    return false;
+  }
+  //NAME's length
+  in_File.read((char*) &SubLength, 4);
+  if (SubLength>255)
+  {
+    std::cout << "StartScriptRecord::loadFromStream: Error: name is longer than 255 characters.\n";
+    std::cout << "File position: "<<in_File.tellg()<<" bytes.\n";
+    return false;
+  }
+  //read name
+  memset(Buffer, '\0', 256);
+  in_File.read(Buffer, SubLength);
+  if (!in_File.good())
+  {
+    std::cout << "StartScriptRecord::loadFromStream: Error while reading name from stream.\n";
+    std::cout << "File position: "<<in_File.tellg()<<" bytes\n";
+    return false;
+  }
+  Name = std::string(Buffer);
+
+  return in_File.good();
+}

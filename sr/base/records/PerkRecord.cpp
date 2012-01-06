26d04af608e7ac45b838b9be2debc7b2052a22e5
thoronador <thoronador@users.sourceforge.net>
2012-01-06 08:07:37 +0000

    minor adjustment

--- sr/base/records/PerkRecord.cpp
+++ sr/base/records/PerkRecord.cpp
@@ -1,7 +1,7 @@
 /*
  -------------------------------------------------------------------------------
     This file is part of the Skyrim Tools Project.
-    Copyright (C) 2011 Thoronador
+    Copyright (C) 2011, 2012 Thoronador
 
     The Skyrim Tools are free software: you can redistribute them and/or
     modify them under the terms of the GNU General Public License as published
@@ -19,6 +19,7 @@
 */
 
 #include "PerkRecord.h"
+#include <cstring>
 #include <iostream>
 #include "../SR_Constants.h"
 #include "../../../mw/base/HelperIO.h"

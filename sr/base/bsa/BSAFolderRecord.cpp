5c33ebd5401cb424109f1470dce4f63f2483235d
thoronador <thoronador@users.sourceforge.net>
2013-11-07 02:04:32 +0100

    SRTP: fix some license notices

--- sr/base/bsa/BSAFolderRecord.cpp
+++ sr/base/bsa/BSAFolderRecord.cpp
@@ -3,18 +3,18 @@
     This file is part of the Skyrim Tools Project.
     Copyright (C) 2011 Thoronador
 
-    The Skyrim Tools are free software: you can redistribute them and/or
-    modify them under the terms of the GNU General Public License as published
-    by the Free Software Foundation, either version 3 of the License, or
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    The Skyrim Tools are distributed in the hope that they will be useful,
+    This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with the Skyrim Tools.  If not, see <http://www.gnu.org/licenses/>.
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  -------------------------------------------------------------------------------
 */
 
@@ -25,10 +25,10 @@ namespace SRTP
 {
 
 BSAFolderRecord::BSAFolderRecord()
+: nameHash(0),
+  count(0),
+  offset(0)
 {
-  nameHash = 0;
-  count = 0;
-  offset = 0;
 }
 
 BSAFolderRecord::~BSAFolderRecord()
--- sr/formID_finder/main.cpp
+++ sr/formID_finder/main.cpp
@@ -3,18 +3,18 @@
     This file is part of the Skyrim Tools Project.
     Copyright (C) 2011, 2012, 2013 Thoronador
 
-    The Skyrim Tools are free software: you can redistribute them and/or
-    modify them under the terms of the GNU General Public License as published
-    by the Free Software Foundation, either version 3 of the License, or
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    The Skyrim Tools are distributed in the hope that they will be useful,
+    This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with the Skyrim Tools.  If not, see <http://www.gnu.org/licenses/>.
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  -------------------------------------------------------------------------------
 */
 

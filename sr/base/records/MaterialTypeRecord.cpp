cadabe06ddb5806145e295cd96780c6bdd54c2dc
Dirk Stolle <striezel-dev@web.de>
2021-08-06 00:18:20 +0200

    modernize: documentation + formatting for SRTP::MaterialTypeRecord

--- sr/base/records/MaterialTypeRecord.cpp
+++ sr/base/records/MaterialTypeRecord.cpp
@@ -1,7 +1,7 @@
 /*
  -------------------------------------------------------------------------------
     This file is part of the Skyrim Tools Project.
-    Copyright (C) 2012, 2013  Thoronador
+    Copyright (C) 2012, 2013, 2021  Thoronador
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -27,7 +27,7 @@
 namespace SRTP
 {
 
-//constants for flags
+// constants for flags
 const uint32_t MaterialTypeRecord::cFlagStairMaterial = 0x00000001;
 const uint32_t MaterialTypeRecord::cFlagArrowsStick   = 0x00000002;
 
@@ -35,126 +35,121 @@ MaterialTypeRecord::MaterialTypeRecord()
 : BasicRecord(), editorID(""),
   materialName(""),
   parentMaterialFormID(0),
-  //subrecord CNAM
+  // subrecord CNAM
   havokDisplayColorR(0.0f),
   havokDisplayColorG(0.0f),
   havokDisplayColorB(0.0f),
-  //end of subrecord CNAM
-  bouyancy(0.0f), //subrecord BNAM
-  flags(0), //subrecord FNAM
-  havokImpactDataSetID(0) //subrecord HNAM
+  // end of subrecord CNAM
+  buoyancy(0.0f), // subrecord BNAM
+  flags(0), // subrecord FNAM
+  havokImpactDataSetID(0) // subrecord HNAM
 {
-
-}
-
-MaterialTypeRecord::~MaterialTypeRecord()
-{
-  //empty
 }
 
 #ifndef SR_NO_RECORD_EQUALITY
 bool MaterialTypeRecord::equals(const MaterialTypeRecord& other) const
 {
-  return ((equalsBasic(other)) and (editorID==other.editorID)
-      and (materialName==other.materialName) and (parentMaterialFormID==other.parentMaterialFormID)
-      and (havokDisplayColorR==other.havokDisplayColorR)
-      and (havokDisplayColorG==other.havokDisplayColorG)
-      and (havokDisplayColorB==other.havokDisplayColorB)
-      and (bouyancy==other.bouyancy)
-      and (flags==other.flags)
-      and (havokImpactDataSetID==other.havokImpactDataSetID));
+  return ((equalsBasic(other)) && (editorID == other.editorID)
+      && (materialName == other.materialName) && (parentMaterialFormID == other.parentMaterialFormID)
+      && (havokDisplayColorR == other.havokDisplayColorR)
+      && (havokDisplayColorG == other.havokDisplayColorG)
+      && (havokDisplayColorB == other.havokDisplayColorB)
+      && (buoyancy == other.buoyancy)
+      && (flags == other.flags)
+      && (havokImpactDataSetID == other.havokImpactDataSetID));
 }
 #endif
 
 #ifndef SR_UNSAVEABLE_RECORDS
 uint32_t MaterialTypeRecord::getWriteSize() const
 {
-  if (isDeleted()) return 0;
+  if (isDeleted())
+    return 0;
   uint32_t writeSize;
-  writeSize = 4 /* EDID */ +2 /* 2 bytes for length */
-             +editorID.length()+1 /* length of name +1 byte for NUL termination */
-             +4 /* MNAM */ +2 /* 2 bytes for length */
-             +materialName.length()+1 /* length of name +1 byte for NUL termination */
-             +4 /* CNAM */ +2 /* 2 bytes for length */ +12 /* fixed size of 12 bytes */
-             +4 /* BNAM */ +2 /* 2 bytes for length */ + 4 /* fixed size of four bytes */
-             +4 /* FNAM */ +2 /* 2 bytes for length */ + 4 /* fixed size of four bytes */;
-  if (parentMaterialFormID!=0)
-    writeSize = writeSize +4 /* PNAM */ +2 /* 2 bytes for length */ + 4 /* fixed size of four bytes */;
-  if (havokImpactDataSetID!=0)
-    writeSize = writeSize +4 /* HNAM */ +2 /* 2 bytes for length */ + 4 /* fixed size of four bytes */;
+  writeSize = 4 /* EDID */ + 2 /* 2 bytes for length */
+             + editorID.length() + 1 /* length of name +1 byte for NUL termination */
+             + 4 /* MNAM */ + 2 /* 2 bytes for length */
+             + materialName.length() + 1 /* length of name +1 byte for NUL termination */
+             + 4 /* CNAM */ + 2 /* 2 bytes for length */ + 12 /* fixed size of 12 bytes */
+             + 4 /* BNAM */ + 2 /* 2 bytes for length */ +  4 /* fixed size of four bytes */
+             + 4 /* FNAM */ + 2 /* 2 bytes for length */ +  4 /* fixed size of four bytes */;
+  if (parentMaterialFormID != 0)
+    writeSize = writeSize + 4 /* PNAM */ + 2 /* 2 bytes for length */ + 4 /* fixed size of four bytes */;
+  if (havokImpactDataSetID != 0)
+    writeSize = writeSize + 4 /* HNAM */ + 2 /* 2 bytes for length */ + 4 /* fixed size of four bytes */;
 
   return writeSize;
 }
 
 bool MaterialTypeRecord::saveToStream(std::ostream& output) const
 {
-  output.write((const char*) &cMATT, 4);
+  output.write(reinterpret_cast<const char*>(&cMATT), 4);
   if (!saveSizeAndUnknownValues(output, getWriteSize())) return false;
   if (isDeleted()) return true;
 
-  //write EDID
-  output.write((const char*) &cEDID, 4);
-  //EDID's length
+  // write EDID
+  output.write(reinterpret_cast<const char*>(&cEDID), 4);
+  // EDID's length
   uint16_t subLength = editorID.length()+1;
-  output.write((const char*) &subLength, 2);
-  //write editor ID
+  output.write(reinterpret_cast<const char*>(&subLength), 2);
+  // write editor ID
   output.write(editorID.c_str(), subLength);
 
-  if (parentMaterialFormID!=0)
+  if (parentMaterialFormID != 0)
   {
-    //write PNAM
-    output.write((const char*) &cPNAM, 4);
-    //PNAM's length
+    // write PNAM
+    output.write(reinterpret_cast<const char*>(&cPNAM), 4);
+    // PNAM's length
     subLength = 4; /* fixed */
-    output.write((const char*) &subLength, 2);
-    //write parent material's form ID
-    output.write((const char*) &parentMaterialFormID, 4);
-  }//if PNAM
+    output.write(reinterpret_cast<const char*>(&subLength), 2);
+    // write parent material's form ID
+    output.write(reinterpret_cast<const char*>(&parentMaterialFormID), 4);
+  } // if PNAM
 
-  //write MNAM
-  output.write((const char*) &cMNAM, 4);
-  //MNAM's length
+  // write MNAM
+  output.write(reinterpret_cast<const char*>(&cMNAM), 4);
+  // MNAM's length
   subLength = materialName.length()+1;
-  output.write((const char*) &subLength, 2);
-  //write model path
+  output.write(reinterpret_cast<const char*>(&subLength), 2);
+  // write model path
   output.write(materialName.c_str(), subLength);
 
-  //write CNAM
-  output.write((const char*) &cCNAM, 4);
-  //CNAM's length
+  // write CNAM
+  output.write(reinterpret_cast<const char*>(&cCNAM), 4);
+  // CNAM's length
   subLength = 12; /* fixed */
-  output.write((const char*) &subLength, 2);
-  //write havok display colour components
-  output.write((const char*) &havokDisplayColorR, 4);
-  output.write((const char*) &havokDisplayColorG, 4);
-  output.write((const char*) &havokDisplayColorB, 4);
+  output.write(reinterpret_cast<const char*>(&subLength), 2);
+  // write havok display colour components
+  output.write(reinterpret_cast<const char*>(&havokDisplayColorR), 4);
+  output.write(reinterpret_cast<const char*>(&havokDisplayColorG), 4);
+  output.write(reinterpret_cast<const char*>(&havokDisplayColorB), 4);
 
-  //write BNAM
-  output.write((const char*) &cBNAM, 4);
-  //BNAM's length
+  // write BNAM
+  output.write(reinterpret_cast<const char*>(&cBNAM), 4);
+  // BNAM's length
   subLength = 4; /* fixed */
-  output.write((const char*) &subLength, 2);
-  //write bouyancy
-  output.write((const char*) &bouyancy, 4);
+  output.write(reinterpret_cast<const char*>(&subLength), 2);
+  // write buoyancy
+  output.write(reinterpret_cast<const char*>(&buoyancy), 4);
 
-  //write FNAM
-  output.write((const char*) &cFNAM, 4);
-  //FNAM's length
+  // write FNAM
+  output.write(reinterpret_cast<const char*>(&cFNAM), 4);
+  // FNAM's length
   subLength = 4; /* fixed */
-  output.write((const char*) &subLength, 2);
-  //write flags
-  output.write((const char*) &flags, 4);
+  output.write(reinterpret_cast<const char*>(&subLength), 2);
+  // write flags
+  output.write(reinterpret_cast<const char*>(&flags), 4);
 
-  if (havokImpactDataSetID!=0)
+  if (havokImpactDataSetID != 0)
   {
-    //write HNAM
-    output.write((const char*) &cHNAM, 4);
-    //HNAM's length
+    // write HNAM
+    output.write(reinterpret_cast<const char*>(&cHNAM), 4);
+    // HNAM's length
     subLength = 4; /* fixed */
-    output.write((const char*) &subLength, 2);
-    //write havok impact data set's form ID
-    output.write((const char*) &havokImpactDataSetID, 4);
-  }//if HNAM
+    output.write(reinterpret_cast<const char*>(&subLength), 2);
+    // write havok impact data set's form ID
+    output.write(reinterpret_cast<const char*>(&havokImpactDataSetID), 4);
+  } // if HNAM
 
   return output.good();
 }
@@ -165,25 +160,23 @@ bool MaterialTypeRecord::loadFromStream(std::istream& in_File, const bool locali
   uint32_t readSize = 0;
   if (!loadSizeAndUnknownValues(in_File, readSize)) return false;
   if (isDeleted()) return true;
-  uint32_t subRecName;
-  uint16_t subLength;
-  subRecName = subLength = 0;
-  uint32_t bytesRead;
+  uint32_t subRecName = 0;
+  uint16_t subLength = 0;
 
-  //read EDID
-  in_File.read((char*) &subRecName, 4);
-  bytesRead = 4;
-  if (subRecName!=cEDID)
+  // read EDID
+  in_File.read(reinterpret_cast<char*>(&subRecName), 4);
+  uint32_t bytesRead = 4;
+  if (subRecName != cEDID)
   {
     UnexpectedRecord(cEDID, subRecName);
     return false;
   }
-  //EDID's length
-  in_File.read((char*) &subLength, 2);
+  // EDID's length
+  in_File.read(reinterpret_cast<char*>(&subLength), 2);
   bytesRead += 2;
-  if (subLength>511)
+  if (subLength > 511)
   {
-    std::cout <<"Error: sub record EDID of MATT is longer than 511 characters!\n";
+    std::cerr << "Error: sub record EDID of MATT is longer than 511 characters!\n";
     return false;
   }
   //read EDID's stuff
@@ -193,7 +186,7 @@ bool MaterialTypeRecord::loadFromStream(std::istream& in_File, const bool locali
   bytesRead += subLength;
   if (!in_File.good())
   {
-    std::cout << "Error while reading subrecord EDID of MATT!\n";
+    std::cerr << "Error while reading subrecord EDID of MATT!\n";
     return false;
   }
   editorID = std::string(buffer);
@@ -201,97 +194,101 @@ bool MaterialTypeRecord::loadFromStream(std::istream& in_File, const bool locali
   parentMaterialFormID = 0;
   materialName.clear();
   bool hasReadCNAM = false;
-  bool hasReadBNAM = false; bouyancy = 0.0f;
+  bool hasReadBNAM = false;
+  buoyancy = 0.0f;
   bool hasReadFNAM = false;
   havokImpactDataSetID = 0;
-  while (bytesRead<readSize)
+  while (bytesRead < readSize)
   {
-    //read next subrecord's name
-    in_File.read((char*) &subRecName, 4);
+    // read next subrecord's name
+    in_File.read(reinterpret_cast<char*>(&subRecName), 4);
     bytesRead += 4;
     switch(subRecName)
     {
       case cPNAM:
-           if (parentMaterialFormID!=0)
+           if (parentMaterialFormID != 0)
            {
-             std::cout << "Error: MATT seems to have more than one PNAM subrecord!\n";
+             std::cerr << "Error: MATT seems to have more than one PNAM subrecord!\n";
              return false;
            }
-           //load PNAM
+           // load PNAM
            if (!loadUint32SubRecordFromStream(in_File, cPNAM, parentMaterialFormID, false))
            {
              return false;
            }
            bytesRead += 6;
-           //check value
-           if (parentMaterialFormID==0)
+           // check value
+           if (parentMaterialFormID == 0)
            {
-             std::cout << "Error: subrecord PNAM of MATT has value zero!\n";
+             std::cerr << "Error: subrecord PNAM of MATT has value zero!\n";
              return false;
            }
            break;
       case cMNAM:
            if (!materialName.empty())
            {
-             std::cout << "Error: MATT seems to have more than one MNAM subrecord!\n";
+             std::cerr << "Error: MATT seems to have more than one MNAM subrecord!\n";
              return false;
            }
-           //read MNAM
+           // read MNAM
            if (!loadString512FromStream(in_File, materialName, buffer, cMNAM, false, bytesRead))
            {
              return false;
            }
            if (materialName.empty())
            {
-             std::cout << "Error: subrecord MNAM of MATT is empty!\n";
+             std::cerr << "Error: subrecord MNAM of MATT is empty!\n";
              return false;
            }
            break;
       case cCNAM:
            if (hasReadCNAM)
            {
-             std::cout << "Error: MATT seems to have more than one CNAM subrecord!\n";
+             std::cerr << "Error: MATT seems to have more than one CNAM subrecord!\n";
              return false;
            }
-           //CNAM's length
-           in_File.read((char*) &subLength, 2);
+           // CNAM's length
+           in_File.read(reinterpret_cast<char*>(&subLength), 2);
            bytesRead += 2;
-           if (subLength!=12)
+           if (subLength != 12)
            {
-             std::cout << "Error: sub record CNAM of MATT has invalid length ("
-                       << subLength<<" bytes). Should be 12 bytes!\n";
+             std::cerr << "Error: sub record CNAM of MATT has invalid length ("
+                       << subLength << " bytes). Should be 12 bytes!\n";
              return false;
            }
-           //now load CNAM
-           in_File.read((char*) &havokDisplayColorR, 4);
-           in_File.read((char*) &havokDisplayColorG, 4);
-           in_File.read((char*) &havokDisplayColorB, 4);
+           // now load CNAM
+           in_File.read(reinterpret_cast<char*>(&havokDisplayColorR), 4);
+           in_File.read(reinterpret_cast<char*>(&havokDisplayColorG), 4);
+           in_File.read(reinterpret_cast<char*>(&havokDisplayColorB), 4);
            bytesRead += 12;
            if (!in_File.good())
            {
-             std::cout << "Error while reading subrecord CNAM of MATT!\n";
+             std::cerr << "Error while reading subrecord CNAM of MATT!\n";
              return false;
            }
-           //integrity check
-           if ((havokDisplayColorR>1.0f) or (havokDisplayColorR<0.0f))
+           // integrity check
+           if ((havokDisplayColorR > 1.0f) || (havokDisplayColorR < 0.0f))
            {
-             std::cout << "Error while reading subrecord CNAM of MATT: red "
+             std::cerr << "Error while reading subrecord CNAM of MATT: red "
                        << "havok display colour component is not in range "
-                       << "[0;1]! Its current value is "<<havokDisplayColorR<<".\n";
+                       << "[0;1]! Its current value is " << havokDisplayColorR
+                       << ".\n";
              return false;
            }
-           if ((havokDisplayColorG>1.0f) or (havokDisplayColorG<0.0f))
+           if ((havokDisplayColorG > 1.0f) || (havokDisplayColorG < 0.0f))
            {
-             std::cout << "Error while reading subrecord CNAM of MATT: green "
+             std::cerr << "Error while reading subrecord CNAM of MATT: green "
                        << "havok display colour component is not in range "
-                       << "[0;1]! Its current value is "<<havokDisplayColorG<<".\n";
+                       << "[0;1]! Its current value is " << havokDisplayColorG
+                       << ".\n";
              return false;
            }
-           if ((havokDisplayColorB>1.0f) or (havokDisplayColorB<0.0f))
+           if ((havokDisplayColorB > 1.0f) || (havokDisplayColorB < 0.0f))
            {
-             std::cout << "Error while reading subrecord CNAM of MATT: blue "
+             std::cerr << "Error while reading subrecord CNAM of MATT: blue "
                        << "havok display colour component is not in range "
-                       << "[0;1]! Its current value is "<<havokDisplayColorB<<".\n";
+                       << "[0;1]! Its current value is " << havokDisplayColorB
+                       << ".\n";
              return false;
            }
            hasReadCNAM = true;
@@ -299,24 +296,24 @@ bool MaterialTypeRecord::loadFromStream(std::istream& in_File, const bool locali
       case cBNAM:
            if (hasReadBNAM)
            {
-             std::cout << "Error: MATT seems to have more than one BNAM subrecord!\n";
+             std::cerr << "Error: MATT seems to have more than one BNAM subrecord!\n";
              return false;
            }
-           //BNAM's length
-           in_File.read((char*) &subLength, 2);
+           // BNAM's length
+           in_File.read(reinterpret_cast<char*>(&subLength), 2);
            bytesRead += 2;
-           if (subLength!=4)
+           if (subLength != 4)
            {
              std::cout << "Error: sub record BNAM of MATT has invalid length ("
-                       << subLength<<" bytes). Should be four bytes!\n";
+                       << subLength << " bytes). Should be four bytes!\n";
              return false;
            }
-           //now load BNAM
-           in_File.read((char*) &bouyancy, 4);
+           // now load BNAM
+           in_File.read(reinterpret_cast<char*>(&buoyancy), 4);
            bytesRead += 4;
            if (!in_File.good())
            {
-             std::cout << "Error while reading subrecord BNAM of MATT!\n";
+             std::cerr << "Error while reading subrecord BNAM of MATT!\n";
              return false;
            }
            hasReadBNAM = true;
@@ -324,10 +321,10 @@ bool MaterialTypeRecord::loadFromStream(std::istream& in_File, const bool locali
       case cFNAM:
            if (hasReadFNAM)
            {
-             std::cout << "Error: MATT seems to have more than one FNAM subrecord!\n";
+             std::cerr << "Error: MATT seems to have more than one FNAM subrecord!\n";
              return false;
            }
-           //load FNAM
+           // load FNAM
            if (!loadUint32SubRecordFromStream(in_File, cFNAM, flags, false))
            {
              return false;
@@ -336,36 +333,36 @@ bool MaterialTypeRecord::loadFromStream(std::istream& in_File, const bool locali
            hasReadFNAM = true;
            break;
       case cHNAM:
-           if (havokImpactDataSetID!=0)
+           if (havokImpactDataSetID != 0)
            {
              std::cout << "Error: MATT seems to have more than one HNAM subrecord!\n";
              return false;
            }
-           //HNAM's length
+           // HNAM's length
            if (!loadUint32SubRecordFromStream(in_File, cHNAM, havokImpactDataSetID, false))
            {
              return false;
            }
            bytesRead += 6;
-           //check value
-           if (havokImpactDataSetID==0)
+           // check value
+           if (havokImpactDataSetID == 0)
            {
              std::cout << "Error: subrecord HNAM of MATT has value zero!\n";
              return false;
            }
            break;
       default:
-           std::cout << "Error: unexpected record type \""<<IntTo4Char(subRecName)
+           std::cout << "Error: unexpected record type \"" << IntTo4Char(subRecName)
                      << "\" found, but only MNAM, CNAM, BNAM or HNAM are allowed here!\n";
            return false;
            break;
-    }//swi
-  }//while
+    } // swi
+  } // while
 
-  //presence checks
-  if (!(!materialName.empty() and hasReadCNAM and hasReadBNAM and hasReadFNAM))
+  // presence checks
+  if (!(!materialName.empty() && hasReadCNAM && hasReadBNAM && hasReadFNAM))
   {
-    std::cout << "Error: At least one required subrecord of MATT is missing!\n";
+    std::cerr << "Error: At least one required subrecord of MATT is missing!\n";
     return false;
   }
 
@@ -387,4 +384,4 @@ bool MaterialTypeRecord::isStairMaterial() const
   return ((cFlagStairMaterial & flags) != 0);
 }
 
-}//namespace
+} // namespace
--- sr/base/records/MaterialTypeRecord.hpp
+++ sr/base/records/MaterialTypeRecord.hpp
@@ -1,7 +1,7 @@
 /*
  -------------------------------------------------------------------------------
     This file is part of the Skyrim Tools Project.
-    Copyright (C) 2012, 2013  Thoronador
+    Copyright (C) 2012, 2013, 2021  Thoronador
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -27,69 +27,91 @@
 namespace SRTP
 {
 
+/** Holds information about a material type. */
 struct MaterialTypeRecord: public BasicRecord
 {
   public:
-    /* constructor */
+    /** Constructor, creates an empty record. */
     MaterialTypeRecord();
 
-    /* destructor */
-    virtual ~MaterialTypeRecord();
+    /** Destructor. */
+    virtual ~MaterialTypeRecord() = default;
 
     #ifndef SR_NO_RECORD_EQUALITY
-    /* returns true, if the other record contains the same data */
+    /** \brief Checks whether another instances contains the same data.
+     *
+     * \param other   the other record to compare with
+     * \return Returns true, if @other contains the same data as instance.
+     *         Returns false otherwise.
+     */
     bool equals(const MaterialTypeRecord& other) const;
     #endif
 
     #ifndef SR_UNSAVEABLE_RECORDS
-    /* returns the size in bytes that the record's data would occupy in a file
-       stream, NOT including the header data
-    */
+    /** \brief Gets the size in bytes that the record's data would occupy in a file
+     *         stream, NOT including the header data.
+     *
+     * \return Returns the size in bytes that the record would need. Size of the
+     *         header is not included.
+     */
     virtual uint32_t getWriteSize() const;
 
-    /* writes the record to the given output stream and returns true on success
-
-      parameters:
-          output   - the output stream
-    */
+    /** \brief Writes the record to the given output stream.
+     *
+     * \param output  the output stream
+     * \return Returns true on success (record was written to stream).
+     *         Returns false, if an error occurred.
+     */
     virtual bool saveToStream(std::ostream& output) const;
     #endif
 
-    /* loads the record from the given input stream and returns true on success
-
-      parameters:
-          in_File   - the input stream
-          localized - whether the file to read from is localized or not
-          table     - the associated string table for localized files
-    */
+    /** \brief Loads the record from the given input stream.
+     *
+     * \param in_File    the input stream
+     * \param localized  whether the file to read from is localized or not
+     * \param table      the associated string table for localized files
+     * \return Returns true on success (record was loaded from stream).
+     *         Returns false, if an error occurred.
+     */
     virtual bool loadFromStream(std::istream& in_File, const bool localized, const StringTable& table);
 
-    /* returns the record's type, usually its header */
+    /** \brief Gets the record's type, usually its header.
+     *
+     * \return Returns the record's type.
+     */
     virtual uint32_t getRecordType() const;
 
-    /* returns true, if arrows stick to this material (according to flags) */
+    /** \brief Checks whether arrows stick to this material (according to flags).
+     *
+     * \return Returns true, if arrows stick to this material.
+     *         Returns false otherwise.
+     */
     bool arrowsStick() const;
 
-    /* returns true, if this material is stair material (according to flags) */
+    /** \brief Checks whether this material is stair material (according to flags).
+     *
+     * \return Returns true, if this material is stair material.
+     *         Returns false otherwise.
+     */
     bool isStairMaterial() const;
 
-    //constants for flags
+    // constants for flags
     static const uint32_t cFlagStairMaterial;
     static const uint32_t cFlagArrowsStick;
 
     std::string editorID;
-    std::string materialName; //subrecord MNAM
-    uint32_t parentMaterialFormID; //subrecord PNAM
-    //subrecord CNAM
+    std::string materialName; // subrecord MNAM
+    uint32_t parentMaterialFormID; // subrecord PNAM
+    // subrecord CNAM
     float havokDisplayColorR;
     float havokDisplayColorG;
     float havokDisplayColorB;
-    //end of subrecord CNAM
-    float bouyancy; //subrecord BNAM
-    uint32_t flags; //subrecord FNAM
-    uint32_t havokImpactDataSetID; //subrecord HNAM
-}; //struct
+    // end of subrecord CNAM
+    float buoyancy; // subrecord BNAM
+    uint32_t flags; // subrecord FNAM
+    uint32_t havokImpactDataSetID; // subrecord HNAM
+}; // struct
 
-} //namespace
+} // namespace
 
 #endif // SR_MATERIALTYPERECORD_HPP

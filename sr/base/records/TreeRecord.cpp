758e7569da7be0a6e04598f60df869f2e90d5c2c
Dirk Stolle <striezel-dev@web.de>
2021-09-10 10:43:08 +0200

    tests: SRTP::TreeRecord

--- sr/base/records/TreeRecord.cpp
+++ sr/base/records/TreeRecord.cpp
@@ -1,7 +1,7 @@
 /*
  -------------------------------------------------------------------------------
     This file is part of the Skyrim Tools Project.
-    Copyright (C) 2012, 2013  Thoronador
+    Copyright (C) 2012, 2013, 2021  Thoronador
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -62,8 +62,9 @@ bool TreeRecord::equals(const TreeRecord& other) const
 #ifndef SR_UNSAVEABLE_RECORDS
 uint32_t TreeRecord::getWriteSize() const
 {
-  uint32_t writeSize;
-  writeSize = 4 /* EDID */ +2 /* 2 bytes for length */
+  if (isDeleted())
+    return 0;
+  uint32_t writeSize = 4 /* EDID */ +2 /* 2 bytes for length */
         +editorID.length()+1 /* length of name +1 byte for NUL termination */
         +4 /* OBND */ +2 /* 2 bytes for length */ +12 /* fixed size */
         +4 /* PFPC */ +2 /* 2 bytes for length */ +4 /* fixed size */
@@ -95,7 +96,10 @@ uint32_t TreeRecord::getWriteSize() const
 bool TreeRecord::saveToStream(std::ostream& output) const
 {
   output.write((const char*) &cTREE, 4);
-  if (!saveSizeAndUnknownValues(output, getWriteSize())) return false;
+  if (!saveSizeAndUnknownValues(output, getWriteSize()))
+    return false;
+  if (isDeleted())
+    return true;
 
   //write EDID
   output.write((const char*) &cEDID, 4);
@@ -185,7 +189,10 @@ bool TreeRecord::saveToStream(std::ostream& output) const
 bool TreeRecord::loadFromStream(std::istream& in_File, const bool localized, const StringTable& table)
 {
   uint32_t readSize = 0;
-  if (!loadSizeAndUnknownValues(in_File, readSize)) return false;
+  if (!loadSizeAndUnknownValues(in_File, readSize))
+    return false;
+  if (isDeleted())
+    return true;
   uint32_t subRecName;
   uint16_t subLength;
   subRecName = subLength = 0;

Thoronador/morrowtools

commit 3d949b946ece38e218b1174afba46a6bdebd9957

tests: SRTP::SpellRecord

sr/base/records/SpellRecord.cpp
@@ -1,7 +1,7 @@
 /*
  -------------------------------------------------------------------------------
     This file is part of the Skyrim Tools Project.
-    Copyright (C) 2011, 2012, 2013  Thoronador
+    Copyright (C) 2011, 2012, 2013, 2021  Thoronador
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -105,6 +105,8 @@ bool SpellRecord::equals(const SpellRecord& other) const
 #ifndef SR_UNSAVEABLE_RECORDS
 uint32_t SpellRecord::getWriteSize() const
 {
+  if (isDeleted())
+    return 0;
   uint32_t writeSize;
   writeSize = 4 /* EDID */ +2 /* 2 bytes for length */
         +editorID.length()+1 /* length of name +1 byte for NUL termination */
@@ -134,7 +136,10 @@ uint32_t SpellRecord::getWriteSize() const
 bool SpellRecord::saveToStream(std::ostream& output) const
 {
   output.write((const char*) &cSPEL, 4);
-  if (!saveSizeAndUnknownValues(output, getWriteSize())) return false;
+  if (!saveSizeAndUnknownValues(output, getWriteSize()))
+    return false;
+  if (isDeleted())
+    return true;
 
   //write EDID
   output.write((const char*) &cEDID, 4);
@@ -218,7 +223,10 @@ bool SpellRecord::saveToStream(std::ostream& output) const
 bool SpellRecord::loadFromStream(std::istream& in_File, const bool localized, const StringTable& table)
 {
   uint32_t readSize = 0;
-  if (!loadSizeAndUnknownValues(in_File, readSize)) return false;
+  if (!loadSizeAndUnknownValues(in_File, readSize))
+    return false;
+  if (isDeleted())
+    return true;
   uint32_t subRecName;
   uint16_t subLength;
   subRecName = subLength = 0;
@@ -280,7 +288,7 @@ bool SpellRecord::loadFromStream(std::istream& in_File, const bool localized, co
 
   name.reset();
   menuDisplayObjectFormID = 0;
-  bool hasReadETYP = false;
+  equipTypeFormID = 0;
   description.reset();
   bool hasReadSPIT = false;
   effects.clear();
@@ -311,7 +319,8 @@ bool SpellRecord::loadFromStream(std::istream& in_File, const bool localized, co
              return false;
            }
            //read MDOB
-           if (!loadUint32SubRecordFromStream(in_File, cMDOB, menuDisplayObjectFormID, false)) return false;
+           if (!loadUint32SubRecordFromStream(in_File, cMDOB, menuDisplayObjectFormID, false))
+             return false;
            bytesRead += 6;
            if (menuDisplayObjectFormID==0)
            {
@@ -320,15 +329,19 @@ bool SpellRecord::loadFromStream(std::istream& in_File, const bool localized, co
            }
            break;
       case cETYP:
-           if (hasReadETYP)
+           if (equipTypeFormID != 0)
            {
              std::cerr << "Error: SPEL seems to have more than one ETYP subrecord!\n";
              return false;
            }
-           //read ETYP
-           if (!loadUint32SubRecordFromStream(in_File, cETYP, equipTypeFormID, false)) return false;
+           if (!loadUint32SubRecordFromStream(in_File, cETYP, equipTypeFormID, false))
+             return false;
            bytesRead += 6;
-           hasReadETYP = true;
+           if (equipTypeFormID == 0)
+           {
+             std::cerr << "Error: subrecord ETYP of SPEL has value zero!\n";
+             return false;
+           }
            break;
       case cDESC:
            if (description.isPresent())
@@ -491,7 +504,7 @@ bool SpellRecord::loadFromStream(std::istream& in_File, const bool localized, co
   }
 
   //check presence
-  if (!(hasReadETYP and description.isPresent() and hasReadSPIT))
+  if (!((equipTypeFormID != 0) and description.isPresent() and hasReadSPIT))
   {
     std::cerr << "Error while reading record SPEL: at least one of the "
               << "subrecords ETYP, DESC or SPIT is missing!\n";

tests/components/CMakeLists.txt
@@ -40,6 +40,7 @@ set(component_tests_sources
     ../../sr/base/records/ShoutRecord.cpp
     ../../sr/base/records/SimplifiedReferenceRecord.cpp
     ../../sr/base/records/SoulGemRecord.cpp
+    ../../sr/base/records/SpellRecord.cpp
     ../../sr/base/records/TalkingActivatorRecord.cpp
     ../../sr/base/records/TES4HeaderRecord.cpp
     ../../sr/base/records/TreeRecord.cpp
@@ -81,6 +82,7 @@ set(component_tests_sources
     sr/base/records/ShoutRecord.cpp
     sr/base/records/SimplifiedReferenceRecord.cpp
     sr/base/records/SoulGemRecord.cpp
+    sr/base/records/SpellRecord.cpp
     sr/base/records/TalkingActivatorRecord.cpp
     sr/base/records/Tes4HeaderRecord.cpp
     sr/base/records/TreeRecord.cpp

tests/components/component-tests.cbp
@@ -103,6 +103,8 @@
 		<Unit filename="../../sr/base/records/SimplifiedReferenceRecord.hpp" />
 		<Unit filename="../../sr/base/records/SoulGemRecord.cpp" />
 		<Unit filename="../../sr/base/records/SoulGemRecord.hpp" />
+		<Unit filename="../../sr/base/records/SpellRecord.cpp" />
+		<Unit filename="../../sr/base/records/SpellRecord.hpp" />
 		<Unit filename="../../sr/base/records/SubBlock.hpp" />
 		<Unit filename="../../sr/base/records/TES4HeaderRecord.cpp" />
 		<Unit filename="../../sr/base/records/TES4HeaderRecord.hpp" />
@@ -150,6 +152,7 @@
 		<Unit filename="sr/base/records/ShoutRecord.cpp" />
 		<Unit filename="sr/base/records/SimplifiedReferenceRecord.cpp" />
 		<Unit filename="sr/base/records/SoulGemRecord.cpp" />
+		<Unit filename="sr/base/records/SpellRecord.cpp" />
 		<Unit filename="sr/base/records/TalkingActivatorRecord.cpp" />
 		<Unit filename="sr/base/records/Tes4HeaderRecord.cpp" />
 		<Unit filename="sr/base/records/TreeRecord.cpp" />

commit c55de117a1972c1a6f0e4fc263ab4831850cc4ae

fix a bug in SpellRecord::loadFromStream() for Dragonborn.esm
The commit 3d949b946ece38e218b1174afba46a6bdebd9957 introduced a
bug when reading SPEL records that are marked as deleted but have
a non-zero size. Some "deleted" spell records still seem to have
some data, although they should not.

sr/base/records/SpellRecord.cpp
@@ -202,7 +202,13 @@ bool SpellRecord::loadFromStream(std::istream& in_File, const bool localized, co
   uint32_t readSize = 0;
   if (!loadSizeAndUnknownValues(in_File, readSize))
     return false;
-  if (isDeleted())
+  // There is at least one deleted spell record in Dragonborn.esm (with the
+  // editor ID MGRSummonDremoraCOPY0000) that has data after the header, so
+  // we check the size value instead of using isDeleted(). Otherwise loading
+  // this version of Dragonborn.esm fails.
+  // Question: Should this approach be used in other record types, too?
+  // This issue needs more research to get a reliable answer.
+  if (readSize == 0)
     return true;
   uint32_t subRecName = 0;
   uint16_t subLength = 0;
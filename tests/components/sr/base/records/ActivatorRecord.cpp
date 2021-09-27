8ef4b57f6cfdf3c1a1a7f57c4f16a329fc48f171
Dirk Stolle <striezel-dev@web.de>
2021-09-27 07:18:03 +0200

    tests: add more tests for SRTP::ActivatorRecord

--- tests/components/sr/base/records/ActivatorRecord.cpp
+++ tests/components/sr/base/records/ActivatorRecord.cpp
@@ -538,7 +538,6 @@ TEST_CASE("ActivatorRecord")
       REQUIRE( record.keywords.size() == 2 );
       REQUIRE( record.keywords[0] == 0x0006DEAD );
       REQUIRE( record.keywords[1] == 0x00100769 );
-      // PNAM\x04\0\xCC\x4C\x33\0
       REQUIRE( record.defaultPrimitiveColour.has_value() );
       REQUIRE( record.defaultPrimitiveColour.value().red == 0xCC );
       REQUIRE( record.defaultPrimitiveColour.value().green == 0x4C );
@@ -550,6 +549,130 @@ TEST_CASE("ActivatorRecord")
       REQUIRE( record.unknownFNAM.has_value() );
       REQUIRE( record.unknownFNAM.value() == 0 );
       REQUIRE( record.interactionKeywordFormID == 0 );
+
+      // Writing should succeed.
+      std::ostringstream streamOut;
+      REQUIRE( record.saveToStream(streamOut) );
+      // Check written data.
+      REQUIRE( streamOut.str() == data );
+    }
+
+    SECTION("default: load record with SNAM and WNAM")
+    {
+      const auto data = "ACTI\x88\0\0\0\0\0\x01\0\x8E\x60\x01\0\x04\x02\x3A\0\x1F\0\x04\0EDID\x1D\0TundraStreamStraight01WaterA\0OBND\x0C\0\x33\xFE\"\xFB\xCB\xFF\x91\x01\x8B\x04\x83\0MODL\x27\0Water\\TundraStreamStraight01WaterA.nif\0PNAM\x04\0\xCCL3\0SNAM\x04\0\xA7\xD0\x03\0WNAM\x04\0)T\x01\0FNAM\x02\0\x01\0"sv;
+      std::istringstream stream;
+      stream.str(std::string(data));
+
+      // read ACTI, because header is handled before loadFromStream.
+      stream.read(reinterpret_cast<char*>(&dummy), 4);
+      REQUIRE( stream.good() );
+
+      // Reading should succeed.
+      ActivatorRecord record;
+      REQUIRE( record.loadFromStream(stream, true, dummy_table) );
+      // Check data.
+      // -- header
+      REQUIRE( record.headerFlags == 0x00010000 );
+      REQUIRE( record.headerFormID == 0x0001608E );
+      REQUIRE( record.headerRevision == 0x003A0204 );
+      REQUIRE( record.headerVersion == 31 );
+      REQUIRE( record.headerUnknown5 == 0x0004 );
+      // -- record data
+      REQUIRE( record.editorID == "TundraStreamStraight01WaterA" );
+      REQUIRE_FALSE( record.unknownVMAD.isPresent() );
+      REQUIRE( record.unknownOBND[0] == 0x33 );
+      REQUIRE( record.unknownOBND[1] == 0xFE );
+      REQUIRE( record.unknownOBND[2] == 0x22 );
+      REQUIRE( record.unknownOBND[3] == 0xFB );
+      REQUIRE( record.unknownOBND[4] == 0xCB );
+      REQUIRE( record.unknownOBND[5] == 0xFF );
+      REQUIRE( record.unknownOBND[6] == 0x91 );
+      REQUIRE( record.unknownOBND[7] == 0x01 );
+      REQUIRE( record.unknownOBND[8] == 0x8B );
+      REQUIRE( record.unknownOBND[9] == 0x04 );
+      REQUIRE( record.unknownOBND[10] == 0x83 );
+      REQUIRE( record.unknownOBND[11] == 0x00 );
+      REQUIRE_FALSE( record.name.isPresent() );
+      REQUIRE( record.modelPath == "Water\\TundraStreamStraight01WaterA.nif" );
+      REQUIRE_FALSE( record.unknownMODT.isPresent() );
+      REQUIRE_FALSE( record.unknownMODS.isPresent() );
+      REQUIRE_FALSE( record.unknownDEST.has_value() );
+      REQUIRE( record.destructionStructures.empty() );
+      REQUIRE( record.keywords.empty() );
+      REQUIRE( record.defaultPrimitiveColour.has_value() );
+      REQUIRE( record.defaultPrimitiveColour.value().red == 0xCC );
+      REQUIRE( record.defaultPrimitiveColour.value().green == 0x4C );
+      REQUIRE( record.defaultPrimitiveColour.value().blue == 0x33 );
+      REQUIRE( record.loopingSoundFormID == 0x0003D0A7 );
+      REQUIRE( record.activateSoundFormID == 0 );
+      REQUIRE( record.waterTypeFormID == 0x00015429 );
+      REQUIRE_FALSE( record.activateTextOverride.isPresent() );
+      REQUIRE( record.unknownFNAM.has_value() );
+      REQUIRE( record.unknownFNAM.value() == 1 );
+      REQUIRE( record.interactionKeywordFormID == 0 );
+
+      // Writing should succeed.
+      std::ostringstream streamOut;
+      REQUIRE( record.saveToStream(streamOut) );
+      // Check written data.
+      REQUIRE( streamOut.str() == data );
+    }
+
+    SECTION("default: load record with KNAM")
+    {
+      const auto data = "ACTI\x8C\0\0\0\0\0\0\0\x45\xF2\x02\0\x13\x64\x36\0\x23\0\x07\0EDID\x19\0FXcameraAttachEffectsACT\0VMAD\x39\0\x04\0\x01\0\x01\0\x14\0FXCameraAttachScript\0\x01\0\x0E\0CameraAttachFX\x01\x01-Y\x05\0\xFF\xFF\0\0OBND\x0C\0\xA8\xFD\xF9\xFD\x9B\xFDX\x02\x07\x02\x65\x02PNAM\x04\0\xCCL3\0FNAM\x02\0\0\0KNAM\x04\0\x06\x93\x09\0"sv;
+      std::istringstream stream;
+      stream.str(std::string(data));
+
+      // read ACTI, because header is handled before loadFromStream.
+      stream.read(reinterpret_cast<char*>(&dummy), 4);
+      REQUIRE( stream.good() );
+
+      // Reading should succeed.
+      ActivatorRecord record;
+      REQUIRE( record.loadFromStream(stream, true, dummy_table) );
+      // Check data.
+      // -- header
+      REQUIRE( record.headerFlags == 0 );
+      REQUIRE( record.headerFormID == 0x0002F245 );
+      REQUIRE( record.headerRevision == 0x00366413 );
+      REQUIRE( record.headerVersion == 35 );
+      REQUIRE( record.headerUnknown5 == 0x0007 );
+      // -- record data
+      REQUIRE( record.editorID == "FXcameraAttachEffectsACT" );
+      REQUIRE( record.unknownVMAD.isPresent() );
+      const auto VMAD = std::string_view(reinterpret_cast<const char*>(record.unknownVMAD.data()), record.unknownVMAD.size());
+      REQUIRE( VMAD == "\x04\0\x01\0\x01\0\x14\0FXCameraAttachScript\0\x01\0\x0E\0CameraAttachFX\x01\x01-Y\x05\0\xFF\xFF\0\0"sv );
+      REQUIRE( record.unknownOBND[0] == 0xA8 );
+      REQUIRE( record.unknownOBND[1] == 0xFD );
+      REQUIRE( record.unknownOBND[2] == 0xF9 );
+      REQUIRE( record.unknownOBND[3] == 0xFD );
+      REQUIRE( record.unknownOBND[4] == 0x9B );
+      REQUIRE( record.unknownOBND[5] == 0xFD );
+      REQUIRE( record.unknownOBND[6] == 0x58 );
+      REQUIRE( record.unknownOBND[7] == 0x02 );
+      REQUIRE( record.unknownOBND[8] == 0x07 );
+      REQUIRE( record.unknownOBND[9] == 0x02 );
+      REQUIRE( record.unknownOBND[10] == 0x65 );
+      REQUIRE( record.unknownOBND[11] == 0x02 );
+      REQUIRE_FALSE( record.name.isPresent() );
+      REQUIRE( record.modelPath.empty() );
+      REQUIRE_FALSE( record.unknownMODT.isPresent() );
+      REQUIRE_FALSE( record.unknownMODS.isPresent() );
+      REQUIRE_FALSE( record.unknownDEST.has_value() );
+      REQUIRE( record.destructionStructures.empty() );
+      REQUIRE( record.keywords.empty() );
+      REQUIRE( record.defaultPrimitiveColour.has_value() );
+      REQUIRE( record.defaultPrimitiveColour.value().red == 0xCC );
+      REQUIRE( record.defaultPrimitiveColour.value().green == 0x4C );
+      REQUIRE( record.defaultPrimitiveColour.value().blue == 0x33 );
+      REQUIRE( record.loopingSoundFormID == 0 );
+      REQUIRE( record.activateSoundFormID == 0 );
+      REQUIRE( record.waterTypeFormID == 0 );
+      REQUIRE_FALSE( record.activateTextOverride.isPresent() );
+      REQUIRE( record.unknownFNAM.has_value() );
+      REQUIRE( record.unknownFNAM.value() == 0 );
+      REQUIRE( record.interactionKeywordFormID == 0x00099306 );
 
       // Writing should succeed.
       std::ostringstream streamOut;
@@ -1096,6 +1219,96 @@ TEST_CASE("ActivatorRecord")
       REQUIRE_FALSE( record.loadFromStream(stream, true, dummy_table) );
     }
 
+    SECTION("corrupt data: multiple SNAMs")
+    {
+      const auto data = "ACTI\x92\0\0\0\0\0\x01\0\x8E\x60\x01\0\x04\x02\x3A\0\x1F\0\x04\0EDID\x1D\0TundraStreamStraight01WaterA\0OBND\x0C\0\x33\xFE\"\xFB\xCB\xFF\x91\x01\x8B\x04\x83\0MODL\x27\0Water\\TundraStreamStraight01WaterA.nif\0PNAM\x04\0\xCCL3\0SNAM\x04\0\xA7\xD0\x03\0SNAM\x04\0\xA7\xD0\x03\0WNAM\x04\0)T\x01\0FNAM\x02\0\x01\0"sv;
+      std::istringstream stream;
+      stream.str(std::string(data));
+
+      // read ACTI, because header is handled before loadFromStream.
+      stream.read(reinterpret_cast<char*>(&dummy), 4);
+      REQUIRE( stream.good() );
+
+      // Reading should fail.
+      ActivatorRecord record;
+      REQUIRE_FALSE( record.loadFromStream(stream, true, dummy_table) );
+    }
+
+    SECTION("corrupt data: stream ends before all of SNAM can be read")
+    {
+      const auto data = "ACTI\x88\0\0\0\0\0\x01\0\x8E\x60\x01\0\x04\x02\x3A\0\x1F\0\x04\0EDID\x1D\0TundraStreamStraight01WaterA\0OBND\x0C\0\x33\xFE\"\xFB\xCB\xFF\x91\x01\x8B\x04\x83\0MODL\x27\0Water\\TundraStreamStraight01WaterA.nif\0PNAM\x04\0\xCCL3\0SNAM\x04\0\xA7\xD0"sv;
+      std::istringstream stream;
+      stream.str(std::string(data));
+
+      // read ACTI, because header is handled before loadFromStream.
+      stream.read(reinterpret_cast<char*>(&dummy), 4);
+      REQUIRE( stream.good() );
+
+      // Reading should fail.
+      ActivatorRecord record;
+      REQUIRE_FALSE( record.loadFromStream(stream, true, dummy_table) );
+    }
+
+    SECTION("corrupt data: SNAM is zero")
+    {
+      const auto data = "ACTI\x88\0\0\0\0\0\x01\0\x8E\x60\x01\0\x04\x02\x3A\0\x1F\0\x04\0EDID\x1D\0TundraStreamStraight01WaterA\0OBND\x0C\0\x33\xFE\"\xFB\xCB\xFF\x91\x01\x8B\x04\x83\0MODL\x27\0Water\\TundraStreamStraight01WaterA.nif\0PNAM\x04\0\xCCL3\0SNAM\x04\0\0\0\0\0WNAM\x04\0)T\x01\0FNAM\x02\0\x01\0"sv;
+      std::istringstream stream;
+      stream.str(std::string(data));
+
+      // read ACTI, because header is handled before loadFromStream.
+      stream.read(reinterpret_cast<char*>(&dummy), 4);
+      REQUIRE( stream.good() );
+
+      // Reading should fail.
+      ActivatorRecord record;
+      REQUIRE_FALSE( record.loadFromStream(stream, true, dummy_table) );
+    }
+
+    SECTION("corrupt data: multiple WNAMs")
+    {
+      const auto data = "ACTI\x92\0\0\0\0\0\x01\0\x8E\x60\x01\0\x04\x02\x3A\0\x1F\0\x04\0EDID\x1D\0TundraStreamStraight01WaterA\0OBND\x0C\0\x33\xFE\"\xFB\xCB\xFF\x91\x01\x8B\x04\x83\0MODL\x27\0Water\\TundraStreamStraight01WaterA.nif\0PNAM\x04\0\xCCL3\0SNAM\x04\0\xA7\xD0\x03\0WNAM\x04\0)T\x01\0WNAM\x04\0)T\x01\0FNAM\x02\0\x01\0"sv;
+      std::istringstream stream;
+      stream.str(std::string(data));
+
+      // read ACTI, because header is handled before loadFromStream.
+      stream.read(reinterpret_cast<char*>(&dummy), 4);
+      REQUIRE( stream.good() );
+
+      // Reading should fail.
+      ActivatorRecord record;
+      REQUIRE_FALSE( record.loadFromStream(stream, true, dummy_table) );
+    }
+
+    SECTION("corrupt data: stream ends before all of WNAM can be read")
+    {
+      const auto data = "ACTI\x88\0\0\0\0\0\x01\0\x8E\x60\x01\0\x04\x02\x3A\0\x1F\0\x04\0EDID\x1D\0TundraStreamStraight01WaterA\0OBND\x0C\0\x33\xFE\"\xFB\xCB\xFF\x91\x01\x8B\x04\x83\0MODL\x27\0Water\\TundraStreamStraight01WaterA.nif\0PNAM\x04\0\xCCL3\0SNAM\x04\0\xA7\xD0\x03\0WNAM\x04\0)T"sv;
+      std::istringstream stream;
+      stream.str(std::string(data));
+
+      // read ACTI, because header is handled before loadFromStream.
+      stream.read(reinterpret_cast<char*>(&dummy), 4);
+      REQUIRE( stream.good() );
+
+      // Reading should fail.
+      ActivatorRecord record;
+      REQUIRE_FALSE( record.loadFromStream(stream, true, dummy_table) );
+    }
+
+    SECTION("corrupt data: WNAM is zero")
+    {
+      const auto data = "ACTI\x88\0\0\0\0\0\x01\0\x8E\x60\x01\0\x04\x02\x3A\0\x1F\0\x04\0EDID\x1D\0TundraStreamStraight01WaterA\0OBND\x0C\0\x33\xFE\"\xFB\xCB\xFF\x91\x01\x8B\x04\x83\0MODL\x27\0Water\\TundraStreamStraight01WaterA.nif\0PNAM\x04\0\xCCL3\0SNAM\x04\0\xA7\xD0\x03\0WNAM\x04\0\0\0\0\0FNAM\x02\0\x01\0"sv;
+      std::istringstream stream;
+      stream.str(std::string(data));
+
+      // read ACTI, because header is handled before loadFromStream.
+      stream.read(reinterpret_cast<char*>(&dummy), 4);
+      REQUIRE( stream.good() );
+
+      // Reading should fail.
+      ActivatorRecord record;
+      REQUIRE_FALSE( record.loadFromStream(stream, true, dummy_table) );
+    }
+
     SECTION("corrupt data: multiple FNAMs")
     {
       const std::string_view data = "ACTI\x04\x01\0\0\0\0\x12\0\x13\x15\x02\0\x1B\x69\x55\0\x28\0\x02\0EDID\x0B\0NorLever01\0VMAD\x3C\0\x05\0\x02\0\x02\0\x09\0TrapLever\0\x01\0\x0B\0TriggerType\x03\x01\x02\0\0\0\x10\0NorLever01SCRIPT\0\0\0OBND\x0C\0\xD2\xFF\xEC\xFF\0\0\x2B\0\x14\0\x5E\0FULL\x04\0\xDA\x0F\0\0MODL\x33\0Dungeons\\Nordic\\Levers\\RuinsLever01\\NorLever01.nif\0MODT\x24\0\x02\0\0\0\x02\0\0\0\0\0\0\0\xB7\x4D\x67\xC4\x64\x64\x73\0\xE4\xF2\xA5\x13\xF1\xCC\xC0\x34\x64\x64\x73\0\xE4\xF2\xA5\x13KSIZ\x04\0\x02\0\0\0KWDA\x08\0\xAD\xDE\x06\0\x69\x07\x10\0PNAM\x04\0\xCC\x4C\x33\0FNAM\x02\0\0\0FNAM\x02\0\0\0"sv;
@@ -1145,6 +1358,51 @@ TEST_CASE("ActivatorRecord")
     SECTION("corrupt data: stream ends before all of FNAM can be read")
     {
       const std::string_view data = "ACTI\xFC\0\0\0\0\0\x12\0\x13\x15\x02\0\x1B\x69\x55\0\x28\0\x02\0EDID\x0B\0NorLever01\0VMAD\x3C\0\x05\0\x02\0\x02\0\x09\0TrapLever\0\x01\0\x0B\0TriggerType\x03\x01\x02\0\0\0\x10\0NorLever01SCRIPT\0\0\0OBND\x0C\0\xD2\xFF\xEC\xFF\0\0\x2B\0\x14\0\x5E\0FULL\x04\0\xDA\x0F\0\0MODL\x33\0Dungeons\\Nordic\\Levers\\RuinsLever01\\NorLever01.nif\0MODT\x24\0\x02\0\0\0\x02\0\0\0\0\0\0\0\xB7\x4D\x67\xC4\x64\x64\x73\0\xE4\xF2\xA5\x13\xF1\xCC\xC0\x34\x64\x64\x73\0\xE4\xF2\xA5\x13KSIZ\x04\0\x02\0\0\0KWDA\x08\0\xAD\xDE\x06\0\x69\x07\x10\0PNAM\x04\0\xCC\x4C\x33\0FNAM\x02\0\0"sv;
+      std::istringstream stream;
+      stream.str(std::string(data));
+
+      // read ACTI, because header is handled before loadFromStream.
+      stream.read(reinterpret_cast<char*>(&dummy), 4);
+      REQUIRE( stream.good() );
+
+      // Reading should fail.
+      ActivatorRecord record;
+      REQUIRE_FALSE( record.loadFromStream(stream, true, dummy_table) );
+    }
+
+    SECTION("corrupt data: multiple KNAMs")
+    {
+      const auto data = "ACTI\x96\0\0\0\0\0\0\0\x45\xF2\x02\0\x13\x64\x36\0\x23\0\x07\0EDID\x19\0FXcameraAttachEffectsACT\0VMAD\x39\0\x04\0\x01\0\x01\0\x14\0FXCameraAttachScript\0\x01\0\x0E\0CameraAttachFX\x01\x01-Y\x05\0\xFF\xFF\0\0OBND\x0C\0\xA8\xFD\xF9\xFD\x9B\xFDX\x02\x07\x02\x65\x02PNAM\x04\0\xCCL3\0FNAM\x02\0\0\0KNAM\x04\0\x06\x93\x09\0KNAM\x04\0\x06\x93\x09\0"sv;
+      std::istringstream stream;
+      stream.str(std::string(data));
+
+      // read ACTI, because header is handled before loadFromStream.
+      stream.read(reinterpret_cast<char*>(&dummy), 4);
+      REQUIRE( stream.good() );
+
+      // Reading should fail.
+      ActivatorRecord record;
+      REQUIRE_FALSE( record.loadFromStream(stream, true, dummy_table) );
+    }
+
+    SECTION("corrupt data: stream ends before all of KNAM can be read")
+    {
+      const auto data = "ACTI\x8C\0\0\0\0\0\0\0\x45\xF2\x02\0\x13\x64\x36\0\x23\0\x07\0EDID\x19\0FXcameraAttachEffectsACT\0VMAD\x39\0\x04\0\x01\0\x01\0\x14\0FXCameraAttachScript\0\x01\0\x0E\0CameraAttachFX\x01\x01-Y\x05\0\xFF\xFF\0\0OBND\x0C\0\xA8\xFD\xF9\xFD\x9B\xFDX\x02\x07\x02\x65\x02PNAM\x04\0\xCCL3\0FNAM\x02\0\0\0KNAM\x04\0\x06\x93"sv;
+      std::istringstream stream;
+      stream.str(std::string(data));
+
+      // read ACTI, because header is handled before loadFromStream.
+      stream.read(reinterpret_cast<char*>(&dummy), 4);
+      REQUIRE( stream.good() );
+
+      // Reading should fail.
+      ActivatorRecord record;
+      REQUIRE_FALSE( record.loadFromStream(stream, true, dummy_table) );
+    }
+
+    SECTION("corrupt data: KNAM is zero")
+    {
+      const auto data = "ACTI\x8C\0\0\0\0\0\0\0\x45\xF2\x02\0\x13\x64\x36\0\x23\0\x07\0EDID\x19\0FXcameraAttachEffectsACT\0VMAD\x39\0\x04\0\x01\0\x01\0\x14\0FXCameraAttachScript\0\x01\0\x0E\0CameraAttachFX\x01\x01-Y\x05\0\xFF\xFF\0\0OBND\x0C\0\xA8\xFD\xF9\xFD\x9B\xFDX\x02\x07\x02\x65\x02PNAM\x04\0\xCCL3\0FNAM\x02\0\0\0KNAM\x04\0\0\0\0\0"sv;
       std::istringstream stream;
       stream.str(std::string(data));
 

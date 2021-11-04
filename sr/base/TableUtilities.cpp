b8bb615bf8932ef557c56c64d64f7c7d4148fba6
Dirk Stolle <striezel-dev@web.de>
2021-11-04 05:07:21 +0100

    formatting adjustments in SRTP's table utility functions

--- sr/base/TableUtilities.cpp
+++ sr/base/TableUtilities.cpp
@@ -28,83 +28,84 @@
 
 namespace SRTP
 {
-  int getLanguageComponent(const std::string& dataDir, const std::string& pluginName, std::string& languageComponent, std::vector<std::string>& stringTableFiles)
+
+int getLanguageComponent(const std::string& dataDir, const std::string& pluginName, std::string& languageComponent, std::vector<std::string>& stringTableFiles)
+{
+  const std::string lcPluginName = lowerCase(pluginName);
+  const std::string::size_type piNameLength = pluginName.length();
+
+  languageComponent = "";
+  std::string part_path, part_name, part_ext;
+
+  const std::vector<FileEntry> files = getDirectoryFileList(dataDir + "Strings" + MWTP::pathDelimiter);
+  if (files.size() < 3)
   {
-    const std::string lcPluginName = lowerCase(pluginName);
-    const std::string::size_type piNameLength = pluginName.length();
+    std::cerr << "Error: Could not find string table files for " << pluginName << "!\n";
+    return SRTP::rcFileError;
+  }
 
-    languageComponent = "";
-    std::string part_path, part_name, part_ext;
+  std::set<std::string> presentStuff;
 
-    const std::vector<FileEntry> files = getDirectoryFileList(dataDir + "Strings" + MWTP::pathDelimiter);
-    if (files.size() < 3)
+  for (unsigned int i = 0; i < files.size(); ++i)
+  {
+    if ((!files[i].isDirectory) && (lowerCase(files[i].fileName.substr(0, piNameLength + 1)) == lcPluginName + "_"))
     {
-      std::cerr << "Error: Could not find string table files for " << pluginName << "!\n";
-      return SRTP::rcFileError;
-    }
-
-    std::set<std::string> presentStuff;
-
-    for (unsigned int i = 0; i < files.size(); ++i)
-    {
-      if ((!files[i].isDirectory) && (lowerCase(files[i].fileName.substr(0, piNameLength + 1)) == lcPluginName + "_"))
+      splitPathFileExtension(files[i].fileName, MWTP::pathDelimiter, part_path, part_name, part_ext);
+      if ((lowerCaseCompare(part_ext, "dlstrings") == 0) || (lowerCaseCompare(part_ext, "strings") == 0)
+        || (lowerCaseCompare(part_ext, "ilstrings") == 0))
       {
-        splitPathFileExtension(files[i].fileName, MWTP::pathDelimiter, part_path, part_name, part_ext);
-        if ((lowerCaseCompare(part_ext, "dlstrings") == 0) || (lowerCaseCompare(part_ext, "strings") == 0)
-          || (lowerCaseCompare(part_ext, "ilstrings") == 0))
+        // Do we have a language component yet?
+        if (languageComponent.empty())
         {
-          // Do we have a language component yet?
-          if (languageComponent.empty())
+          languageComponent = part_name.substr(piNameLength + 1);
+          presentStuff.insert(dataDir + "Strings" + MWTP::pathDelimiter + files[i].fileName);
+        }
+        else
+        {
+          if (part_name.substr(piNameLength + 1) == languageComponent)
           {
-            languageComponent = part_name.substr(piNameLength + 1);
             presentStuff.insert(dataDir + "Strings" + MWTP::pathDelimiter + files[i].fileName);
           }
-          else
-          {
-            if (part_name.substr(piNameLength + 1) == languageComponent)
-            {
-              presentStuff.insert(dataDir + "Strings" + MWTP::pathDelimiter + files[i].fileName);
-            }
-          }
-        } // if string file extension
-      } // if file name starts with plugin name
-    }
-
-    if (presentStuff.size() < 3)
-    {
-      std::cerr << "Error: Could not find all three string table files!\n";
-      return SRTP::rcFileError;
-    }
-
-    if (presentStuff.size() > 3)
-    {
-      // Could possibly happen on file systems with case-insensitive file names.
-      std::cerr << "Error: Found more than three string table files!\n";
-      return SRTP::rcFileError;
-    }
-
-    stringTableFiles.clear();
-    std::set<std::string>::const_iterator cIter = presentStuff.begin();
-    while (cIter != presentStuff.end())
-    {
-      stringTableFiles.push_back(*cIter);
-      ++cIter;
-    }
-
-    return 0;
+        }
+      } // if string file extension
+    } // if file name starts with plugin name
   }
 
-  int getAssociatedTableFiles(const std::string& fileName, std::vector<std::string>& files)
+  if (presentStuff.size() < 3)
   {
-    std::string part_path, part_name, part_ext;
-    splitPathFileExtension(fileName, MWTP::pathDelimiter, part_path, part_name, part_ext);
-
-    int lc_return = getLanguageComponent(part_path, part_name, part_ext, files);
-    // If return code is not zero, an error occurred! We should return in that case.
-    if (lc_return != 0)
-      return lc_return;
-    else
-      return 0;
+    std::cerr << "Error: Could not find all three string table files!\n";
+    return SRTP::rcFileError;
   }
+
+  if (presentStuff.size() > 3)
+  {
+    // Could possibly happen on file systems with case-insensitive file names.
+    std::cerr << "Error: Found more than three string table files!\n";
+    return SRTP::rcFileError;
+  }
+
+  stringTableFiles.clear();
+  std::set<std::string>::const_iterator cIter = presentStuff.begin();
+  while (cIter != presentStuff.end())
+  {
+    stringTableFiles.push_back(*cIter);
+    ++cIter;
+  }
+
+  return 0;
+}
+
+int getAssociatedTableFiles(const std::string& fileName, std::vector<std::string>& files)
+{
+  std::string part_path, part_name, part_ext;
+  splitPathFileExtension(fileName, MWTP::pathDelimiter, part_path, part_name, part_ext);
+
+  int lc_return = getLanguageComponent(part_path, part_name, part_ext, files);
+  // If return code is not zero, an error occurred! We should return in that case.
+  if (lc_return != 0)
+    return lc_return;
+  else
+    return 0;
+}
 
 } // namespace
--- sr/base/TableUtilities.hpp
+++ sr/base/TableUtilities.hpp
@@ -26,35 +26,34 @@
 
 namespace SRTP
 {
-  /* tries to find the language component of the string file names for a given
-     plugin. Returns an error code not equal to zero, if an error occurred.
-     Returns zero in case of success.
 
-     parameters:
-         dataDir           - path to the data directory of Skyrim
-         pluginName        - name of the plugin (without extension),
-                             e.g. "Skyrim" for Skyrim.esm
-         languageComponent - string that will be used to return the language
-                             component of the string file names
-         stringTableFiles  - vector that will be used to returns the found file
-                             names of the string table files
+/** \brief Tries to find the language component of the string file names for a given
+ * plugin.
+ *
+ * \param dataDir           - path to the data directory of Skyrim
+ * \param pluginName        - name of the plugin (without extension),
+                              e.g. "Skyrim" for Skyrim.esm
+ * \param languageComponent - string that will be used to return the language
+                              component of the string file names
+ * \param stringTableFiles  - vector that will be used to returns the found file
+                              names of the string table files
+ * \return Returns an error code not equal to zero, if an error occurred.
+ *         Returns zero in case of success.
+ * \remarks The value of languageComponent is changed by this function, even
+ *          if an error occurs. However, the value is undefined in that case
+ *          and does not represent a proper value.
+ */
+int getLanguageComponent(const std::string& dataDir, const std::string& pluginName, std::string& languageComponent, std::vector<std::string>& stringTableFiles);
 
-     remarks:
-         The value of languageComponent is changed by this function, even if
-         the an error occurs. However, the value is undefined in that case and
-         does not represent a proper value.
-  */
-  int getLanguageComponent(const std::string& dataDir, const std::string& pluginName, std::string& languageComponent, std::vector<std::string>& stringTableFiles);
+/* retrieves the paths of the associated string table files for a given
+   .esm/.esp file and returns zero in case of success or non-zero return code
+   in case of error
 
-  /* retrieves the paths of the associated string table files for a given
-     .esm/.esp file and returns zero in case of success or non-zero return code
-     in case of error
-
-     parameters:
-         fileName - name of the .esm/.esp file
-         files    - vector that will hold the string table files
-  */
-  int getAssociatedTableFiles(const std::string& fileName, std::vector<std::string>& files);
+   parameters:
+       fileName - name of the .esm/.esp file
+       files    - vector that will hold the string table files
+*/
+int getAssociatedTableFiles(const std::string& fileName, std::vector<std::string>& files);
 
 } // namespace
 

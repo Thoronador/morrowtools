8f81d2b1e26767672575230c0a1d18060f8a2c1e
Dirk Stolle <striezel-dev@web.de>
2021-11-08 18:56:44 +0100

    hex2sv: fix some edge cases getting too much escape sequences

--- tests/hex2sv/src/main.rs
+++ tests/hex2sv/src/main.rs
@@ -41,11 +41,14 @@ fn to_sequence(byte: &u8) -> String
   return format!("\\x{:0>2X}", byte);
 }
 
-fn is_safe_to_push(c: &char, s: &str) -> bool
+fn is_safe_to_push(c: &char, prev_escaped: &bool, prev_nul: &bool) -> bool
 {
-  !c.is_ascii_hexdigit()
-  || (s.chars().rev().nth(3) != Some('\\')
-  && s.chars().rev().nth(2) != Some('x'))
+  // "graphic" characters, except for backslash and double quote, ...
+  c.is_ascii_graphic() && *c != '\\' && *c != '"'
+  // unless the previous character was escaped and the current is a hex digit
+  && (!c.is_ascii_hexdigit() || !prev_escaped)
+  // .. or the last char was NUL and the character is an octal digit
+  && (!c.is_digit(8) || !prev_nul)
 }
 
 fn hex_to_string_view(octets: &str) -> Result<String, String>
@@ -53,6 +56,8 @@ fn hex_to_string_view(octets: &str) -> Result<String, String>
   let mut result = String::with_capacity(octets.len() / 3);
 
   let mut spliterator = octets.split(' ').enumerate();
+  let mut previous_was_escaped = false;
+  let mut previous_was_nul = false;
   loop
   {
     let next = spliterator.next();
@@ -70,21 +75,29 @@ fn hex_to_string_view(octets: &str) -> Result<String, String>
     if index < 4
     {
       result.push(byte as char);
+      previous_was_escaped = false;
+      previous_was_nul = false;
     }
     else if index < 24
     {
       result.push_str(&to_sequence(&byte));
+      previous_was_escaped = byte != 0u8;
+      previous_was_nul = byte == 0u8;
     }
     else
     {
       let c = byte as char;
-      if c.is_ascii_graphic() && c != '\\' && c != '"' && is_safe_to_push(&c, &result)
+      if is_safe_to_push(&c, &previous_was_escaped, &previous_was_nul)
       {
         result.push(c);
+        previous_was_escaped = false;
+        previous_was_nul = false;
       }
       else
       {
         result.push_str(&to_sequence(&byte));
+        previous_was_escaped = byte != 0u8;
+        previous_was_nul = byte == 0u8;
       }
     }
   }
@@ -128,6 +141,124 @@ mod tests
     let expected: Result<String, String> = Ok("AACT\\x11\\0\\0\\0\\0\\0\\0\\0\\x02\\x30\\x01\\0\\x19\\x4B\\x0E\\0\\x0F\\0\\x01\\0EDID\\x0B\\0ActionIdle\\0".to_string());
     let actual = hex_to_string_view(input);
     assert_eq!(actual, expected);
+  }
+
+  #[test]
+  fn test_hex_to_string_view_modl()
+  {
+    let input = "4D 41 54 4F 57 02 00 00 00 00 00 00 26 F8 01 00 0A 50 25 00 12 00 01 00 45 44 49 44 15 00 53 68 61 64 65 72 54 65 73 74 73 53 68 61 64 65 72 42 6F 78 00 4D 4F 44 4C 1A 00 53 68 61 64 65 72 54 65 73 74 73 5C 53 68 61 64 65 72 42 6F 78 2E 6E 69 66 00";
+    let expected: Result<String, String> = Ok("MATO\\x57\\x02\\0\\0\\0\\0\\0\\0\\x26\\xF8\\x01\\0\\x0A\\x50\\x25\\0\\x12\\0\\x01\\0EDID\\x15\\0ShaderTestsShaderBox\\0MODL\\x1A\\0ShaderTests\\\\ShaderBox.nif\\0".to_string());
+    let actual = hex_to_string_view(input);
+    assert_eq!(actual, expected);
+  }
+
+  #[test]
+  fn test_is_safe_to_push_backslash()
+  {
+    assert_eq!(is_safe_to_push(&'\\', &false, &false), false);
+    assert_eq!(is_safe_to_push(&'\\', &false, &true), false);
+    assert_eq!(is_safe_to_push(&'\\', &true, &false), false);
+    assert_eq!(is_safe_to_push(&'\\', &true, &true), false);
+  }
+
+  #[test]
+  fn test_is_safe_to_push_hex()
+  {
+    assert_eq!(is_safe_to_push(&'9', &false, &false), true);
+    assert_eq!(is_safe_to_push(&'9', &false, &true), true);
+    assert_eq!(is_safe_to_push(&'9', &true, &false), false);
+    assert_eq!(is_safe_to_push(&'9', &true, &true), false);
+
+    assert_eq!(is_safe_to_push(&'A', &false, &false), true);
+    assert_eq!(is_safe_to_push(&'A', &false, &true), true);
+    assert_eq!(is_safe_to_push(&'A', &true, &false), false);
+    assert_eq!(is_safe_to_push(&'A', &true, &true), false);
+
+    assert_eq!(is_safe_to_push(&'a', &false, &false), true);
+    assert_eq!(is_safe_to_push(&'a', &false, &true), true);
+    assert_eq!(is_safe_to_push(&'a', &true, &false), false);
+    assert_eq!(is_safe_to_push(&'a', &true, &true), false);
+
+    assert_eq!(is_safe_to_push(&'E', &false, &false), true);
+    assert_eq!(is_safe_to_push(&'E', &false, &true), true);
+    assert_eq!(is_safe_to_push(&'E', &true, &false), false);
+    assert_eq!(is_safe_to_push(&'E', &true, &true), false);
+
+    assert_eq!(is_safe_to_push(&'e', &false, &false), true);
+    assert_eq!(is_safe_to_push(&'e', &false, &true), true);
+    assert_eq!(is_safe_to_push(&'e', &true, &false), false);
+    assert_eq!(is_safe_to_push(&'e', &true, &true), false);
+
+    assert_eq!(is_safe_to_push(&'F', &false, &false), true);
+    assert_eq!(is_safe_to_push(&'F', &false, &true), true);
+    assert_eq!(is_safe_to_push(&'F', &true, &false), false);
+    assert_eq!(is_safe_to_push(&'F', &true, &true), false);
+
+    assert_eq!(is_safe_to_push(&'f', &false, &false), true);
+    assert_eq!(is_safe_to_push(&'f', &false, &true), true);
+    assert_eq!(is_safe_to_push(&'f', &true, &false), false);
+    assert_eq!(is_safe_to_push(&'f', &true, &true), false);
+  }
+
+  #[test]
+  fn test_is_safe_to_push_non_hex()
+  {
+    assert_eq!(is_safe_to_push(&'G', &false, &false), true);
+    assert_eq!(is_safe_to_push(&'G', &false, &true), true);
+    assert_eq!(is_safe_to_push(&'G', &true, &false), true);
+    assert_eq!(is_safe_to_push(&'G', &true, &true), true);
+
+    assert_eq!(is_safe_to_push(&'g', &false, &false), true);
+    assert_eq!(is_safe_to_push(&'g', &false, &true), true);
+    assert_eq!(is_safe_to_push(&'g', &true, &false), true);
+    assert_eq!(is_safe_to_push(&'g', &true, &true), true);
+  }
+
+  #[test]
+  fn test_is_safe_to_push_octal()
+  {
+    assert_eq!(is_safe_to_push(&'0', &false, &false), true);
+    assert_eq!(is_safe_to_push(&'0', &false, &true), false);
+    assert_eq!(is_safe_to_push(&'0', &true, &false), false);
+    assert_eq!(is_safe_to_push(&'0', &true, &true), false);
+
+    assert_eq!(is_safe_to_push(&'1', &false, &false), true);
+    assert_eq!(is_safe_to_push(&'1', &false, &true), false);
+    assert_eq!(is_safe_to_push(&'1', &true, &false), false);
+    assert_eq!(is_safe_to_push(&'1', &true, &true), false);
+
+    assert_eq!(is_safe_to_push(&'5', &false, &false), true);
+    assert_eq!(is_safe_to_push(&'5', &false, &true), false);
+    assert_eq!(is_safe_to_push(&'5', &true, &false), false);
+    assert_eq!(is_safe_to_push(&'5', &true, &true), false);
+
+    assert_eq!(is_safe_to_push(&'6', &false, &false), true);
+    assert_eq!(is_safe_to_push(&'6', &false, &true), false);
+    assert_eq!(is_safe_to_push(&'6', &true, &false), false);
+    assert_eq!(is_safe_to_push(&'6', &true, &true), false);
+
+    assert_eq!(is_safe_to_push(&'7', &false, &false), true);
+    assert_eq!(is_safe_to_push(&'7', &false, &true), false);
+    assert_eq!(is_safe_to_push(&'7', &true, &false), false);
+    assert_eq!(is_safe_to_push(&'7', &true, &true), false);
+  }
+
+  #[test]
+  fn test_is_safe_to_push_nul()
+  {
+    assert_eq!(is_safe_to_push(&'\0', &false, &false), false);
+    assert_eq!(is_safe_to_push(&'\0', &false, &true), false);
+    assert_eq!(is_safe_to_push(&'\0', &true, &false), false);
+    assert_eq!(is_safe_to_push(&'\0', &true, &true), false);
+  }
+
+  #[test]
+  fn test_is_safe_to_push_quote()
+  {
+    assert_eq!(is_safe_to_push(&'"', &false, &false), false);
+    assert_eq!(is_safe_to_push(&'"', &false, &true), false);
+    assert_eq!(is_safe_to_push(&'"', &true, &false), false);
+    assert_eq!(is_safe_to_push(&'"', &true, &true), false);
   }
 
   #[test]

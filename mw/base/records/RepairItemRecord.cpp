1a16a1560b10b557e3a33d9b5665d9f1ee79301c
Dirk Stolle <striezel-dev@web.de>
2021-11-23 02:57:16 +0100

    tests+modernize: MWTP::RepairItemRecord

--- mw/base/records/RepairItemRecord.cpp
+++ mw/base/records/RepairItemRecord.cpp
@@ -1,7 +1,7 @@
 /*
  -------------------------------------------------------------------------------
     This file is part of the Morrowind Tools Project.
-    Copyright (C) 2009, 2011, 2012, 2013  Thoronador
+    Copyright (C) 2009, 2011, 2012, 2013, 2021  Thoronador
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -19,7 +19,6 @@
 */
 
 #include "RepairItemRecord.hpp"
-#include <cstring>
 #include <iostream>
 #include "../MW_Constants.hpp"
 #include "../HelperIO.hpp"
@@ -28,66 +27,50 @@
 namespace MWTP
 {
 
-RepairItemRecord::RepairItemRecord()
-: BasicRecord(),
-  recordID(""), ModelPath(""), Name(""),
-  //repair data
-  Weight(0.0f),
-  Value(0),
-  Uses(0),
-  Quality(0.0f),
-  //end of repair data
-  InventoryIcon(""), ScriptName("")
-{}
-
 RepairItemRecord::RepairItemRecord(const std::string& ID)
 : BasicRecord(),
   recordID(ID),
-  ModelPath(""), Name(""),
-  //repair data
+  ModelPath(std::string()),
+  Name(std::string()),
+  // repair data
   Weight(0.0f),
   Value(0),
   Uses(0),
   Quality(0.0f),
-  //end of repair data
-  InventoryIcon(""), ScriptName("")
+  // end of repair data
+  InventoryIcon(std::string()),
+  ScriptName(std::string())
 {}
-
-RepairItemRecord::~RepairItemRecord()
-{
-  //empty
-}
 
 bool RepairItemRecord::equals(const RepairItemRecord& other) const
 {
-  return ((recordID==other.recordID) and (ModelPath==other.ModelPath)
-      and (Name==other.Name) and (Weight==other.Weight) and (Value==other.Value)
-      and (Uses==other.Uses) and (Quality==other.Quality)
-      and (InventoryIcon==other.InventoryIcon) and (ScriptName==other.ScriptName));
+  return (recordID == other.recordID) && (ModelPath == other.ModelPath)
+      && (Name == other.Name) && (Weight == other.Weight) && (Value == other.Value)
+      && (Uses == other.Uses) && (Quality == other.Quality)
+      && (InventoryIcon == other.InventoryIcon) && (ScriptName == other.ScriptName);
 }
 
 #ifndef MW_UNSAVEABLE_RECORDS
 bool RepairItemRecord::saveToStream(std::ostream& output) const
 {
-  output.write((const char*) &cREPA, 4);
-  uint32_t Size;
-  Size = 4 /* NAME */ +4 /* 4 bytes for length */
-        +recordID.length()+1 /* length of ID +1 byte for NUL termination */
-        +4 /* MODL */ +4 /* 4 bytes for length */
-        +ModelPath.length()+1 /* length of path +1 byte for NUL termination */
-        +4 /* FNAM */ +4 /* 4 bytes for length */
-        +Name.length()+1 /* length of name +1 byte for NUL termination */
-        +4 /* RIDT */ +4 /* RIDT's length */ +16 /*size of RIDT (always 16 bytes)*/
-        +4 /* ITEX */ +4 /* 4 bytes for length */
-        +InventoryIcon.length()+1 /* length of icon path +1 byte for NUL termination */;
+  output.write(reinterpret_cast<const char*>(&cREPA), 4);
+  uint32_t Size = 4 /* NAME */ + 4 /* 4 bytes for length */
+      + recordID.length() + 1 /* length of ID +1 byte for NUL termination */
+      + 4 /* MODL */ + 4 /* 4 bytes for length */
+      + ModelPath.length() + 1 /* length of path +1 byte for NUL termination */
+      + 4 /* FNAM */ + 4 /* 4 bytes for length */
+      + Name.length() + 1 /* length of name +1 byte for NUL termination */
+      + 4 /* RIDT */ + 4 /* RIDT's length */ + 16 /* size (always 16 bytes) */
+      + 4 /* ITEX */ + 4 /* 4 bytes for length */
+      + InventoryIcon.length() + 1 /* length of icon path +1 byte for NUL termination */;
   if (!ScriptName.empty())
   {
-    Size = Size +4 /* SCRIM */ +4 /* 4 bytes for length */
-          +ScriptName.length()+1; /* length of script ID +1 byte for NUL termination */;
+    Size = Size + 4 /* SCRI */ + 4 /* 4 bytes for length */
+          + ScriptName.length() + 1; /* length of script ID +1 byte for NUL termination */;
   }
-  output.write((const char*) &Size, 4);
-  output.write((const char*) &HeaderOne, 4);
-  output.write((const char*) &HeaderFlags, 4);
+  output.write(reinterpret_cast<const char*>(&Size), 4);
+  output.write(reinterpret_cast<const char*>(&HeaderOne), 4);
+  output.write(reinterpret_cast<const char*>(&HeaderFlags), 4);
 
   /*Repair Item:
     NAME = Item ID, required
@@ -101,71 +84,58 @@ bool RepairItemRecord::saveToStream(std::ostream& output) const
     ITEX = Inventory Icon
     SCRI = Script Name (optional) */
 
-  //write NAME
-  output.write((const char*) &cNAME, 4);
-  //NAME's length
-  uint32_t SubLength;
-  SubLength = recordID.length()+1;//length of string plus one for NUL-termination
-  output.write((const char*) &SubLength, 4);
-  //write ID
+  // write item ID (NAME)
+  output.write(reinterpret_cast<const char*>(&cNAME), 4);
+  uint32_t SubLength = recordID.length() + 1;
+  output.write(reinterpret_cast<const char*>(&SubLength), 4);
   output.write(recordID.c_str(), SubLength);
 
-  //write MODL
-  output.write((const char*) &cMODL, 4);
-  //MODL's length
-  SubLength = ModelPath.length()+1;//length of string plus one for NUL-termination
-  output.write((const char*) &SubLength, 4);
-  //write model path
+  // write model path (MODL)
+  output.write(reinterpret_cast<const char*>(&cMODL), 4);
+  SubLength = ModelPath.length() + 1; // length + NUL terminator
+  output.write(reinterpret_cast<const char*>(&SubLength), 4);
   output.write(ModelPath.c_str(), SubLength);
 
-  //write FNAM
-  output.write((const char*) &cFNAM, 4);
-  //FNAM's length
-  SubLength = Name.length()+1;//length of string plus one for NUL-termination
-  output.write((const char*) &SubLength, 4);
-  //write repair item name
+  // write repair item name (FNAM)
+  output.write(reinterpret_cast<const char*>(&cFNAM), 4);
+  SubLength = Name.length() + 1; // length of string + one for NUL-termination
+  output.write(reinterpret_cast<const char*>(&SubLength), 4);
   output.write(Name.c_str(), SubLength);
 
-  //write RIDT
-  output.write((const char*) &cRIDT, 4);
-  //RIDT's length
-  SubLength = 16;//length of RIDT is always 16 bytes
-  output.write((const char*) &SubLength, 4);
-  //write repair item data
-  output.write((const char*) &Weight, 4);
-  output.write((const char*) &Value, 4);
-  output.write((const char*) &Uses, 4);
-  output.write((const char*) &Quality, 4);
+  // write repair item data (RIDT)
+  output.write(reinterpret_cast<const char*>(&cRIDT), 4);
+  SubLength = 16;
+  output.write(reinterpret_cast<const char*>(&SubLength), 4);
+  output.write(reinterpret_cast<const char*>(&Weight), 4);
+  output.write(reinterpret_cast<const char*>(&Value), 4);
+  output.write(reinterpret_cast<const char*>(&Uses), 4);
+  output.write(reinterpret_cast<const char*>(&Quality), 4);
 
-  //write ITEX
-  output.write((const char*) &cITEX, 4);
-  //ITEX's length
-  SubLength = InventoryIcon.length()+1;//length of string plus one for NUL-termination
-  output.write((const char*) &SubLength, 4);
-  //write inventory icon path
+  // write inventory icon path (ITEX)
+  output.write(reinterpret_cast<const char*>(&cITEX), 4);
+  SubLength = InventoryIcon.length() + 1; // length + NUL-terminator
+  output.write(reinterpret_cast<const char*>(&SubLength), 4);
   output.write(InventoryIcon.c_str(), SubLength);
 
   if (!ScriptName.empty())
   {
-    //write SCRI
-    output.write((const char*) &cSCRI, 4);
-    //SCRI's length
-    SubLength = ScriptName.length()+1;//length of string plus one for NUL-termination
-    output.write((const char*) &SubLength, 4);
-    //write script name/ID
+    // write script name / ID (SCRI)
+    output.write(reinterpret_cast<const char*>(&cSCRI), 4);
+    SubLength = ScriptName.length() + 1;
+    output.write(reinterpret_cast<const char*>(&SubLength), 4);
     output.write(ScriptName.c_str(), SubLength);
-  }//if
+  }
 
   return output.good();
 }
 #endif
 
-bool RepairItemRecord::loadFromStream(std::istream& in_File)
+bool RepairItemRecord::loadFromStream(std::istream& input)
 {
-  uint32_t Size;
-  in_File.read((char*) &Size, 4);
-  in_File.read((char*) &HeaderOne, 4);
-  in_File.read((char*) &HeaderFlags, 4);
+  uint32_t Size = 0;
+  input.read(reinterpret_cast<char*>(&Size), 4);
+  input.read(reinterpret_cast<char*>(&HeaderOne), 4);
+  input.read(reinterpret_cast<char*>(&HeaderFlags), 4);
 
   /*Repair Item:
     NAME = Item ID, required
@@ -179,179 +149,83 @@ bool RepairItemRecord::loadFromStream(std::istream& in_File)
     ITEX = Inventory Icon
     SCRI = Script Name (optional) */
 
-  uint32_t SubRecName;
-  uint32_t SubLength, BytesRead;
-  SubRecName = SubLength = 0;
+  uint32_t BytesRead = 0;
 
-  //read NAME
-  in_File.read((char*) &SubRecName, 4);
-  BytesRead = 4;
-  if (SubRecName!=cNAME)
-  {
-    UnexpectedRecord(cNAME, SubRecName);
-    return false;
-  }
-  //NAME's length
-  in_File.read((char*) &SubLength, 4);
-  BytesRead += 4;
-  if (SubLength>255)
-  {
-    std::cout << "Error: subrecord NAME of REPA is longer than 255 characters.\n";
-    return false;
-  }
-  //read repair item ID
+  // read NAME
   char Buffer[256];
-  memset(Buffer, '\0', 256);
-  in_File.read(Buffer, SubLength);
-  BytesRead += SubLength;
-  if (!in_File.good())
+  if (!loadString256WithHeader(input, recordID, Buffer, cNAME, BytesRead))
   {
-    std::cout << "Error while reading subrecord NAME of REPA.\n";
+    std::cerr << "Error while reading subrecord NAME of REPA.\n";
     return false;
   }
-  recordID = std::string(Buffer);
 
-  //read MODL
-  in_File.read((char*) &SubRecName, 4);
-  BytesRead += 4;
-  if (SubRecName!=cMODL)
+  // read MODL
+  if (!loadString256WithHeader(input, ModelPath, Buffer, cMODL, BytesRead))
   {
-    UnexpectedRecord(cMODL, SubRecName);
+    std::cerr << "Error while reading subrecord MODL of REPA.\n";
     return false;
   }
-  //MODL's length
-  in_File.read((char*) &SubLength, 4);
-  BytesRead += 4;
-  if (SubLength>255)
-  {
-    std::cout << "Error: subrecord MODL of REPA is longer than 255 characters.\n";
-    return false;
-  }
-  //read model path
-  memset(Buffer, '\0', 256);
-  in_File.read(Buffer, SubLength);
-  BytesRead += SubLength;
-  if (!in_File.good())
-  {
-    std::cout << "Error while reading subrecord MODL of REPA.\n";
-    return false;
-  }
-  ModelPath = std::string(Buffer);
 
-  //read FNAM
-  in_File.read((char*) &SubRecName, 4);
-  BytesRead += 4;
-  if (SubRecName!=cFNAM)
+  // read FNAM
+  if (!loadString256WithHeader(input, Name, Buffer, cFNAM, BytesRead))
   {
-    UnexpectedRecord(cFNAM, SubRecName);
+    std::cerr << "Error while reading subrecord FNAM of REPA.\n";
     return false;
   }
-  //FNAM's length
-  in_File.read((char*) &SubLength, 4);
-  BytesRead += 4;
-  if (SubLength>255)
-  {
-    std::cout << "Error: subrecord FNAM of REPA is longer than 255 characters.\n";
-    return false;
-  }
-  //read name
-  memset(Buffer, '\0', 256);
-  in_File.read(Buffer, SubLength);
-  BytesRead += SubLength;
-  if (!in_File.good())
-  {
-    std::cout << "Error while reading subrecord FNAM of REPA.\n";
-    return false;
-  }
-  Name = std::string(Buffer);
 
-  //read RIDT
-  in_File.read((char*) &SubRecName, 4);
+  uint32_t SubRecName = 0;
+  // read RIDT
+  input.read(reinterpret_cast<char*>(&SubRecName), 4);
   BytesRead += 4;
-  if (SubRecName!=cRIDT)
+  if (SubRecName != cRIDT)
   {
     UnexpectedRecord(cRIDT, SubRecName);
     return false;
   }
-  //RIDT's length
-  in_File.read((char*) &SubLength, 4);
+  // RIDT's length
+  uint32_t SubLength = 0;
+  input.read(reinterpret_cast<char*>(&SubLength), 4);
   BytesRead += 4;
-  if (SubLength!=16)
+  if (SubLength != 16)
   {
-    std::cout << "Error: sub record RIDT of REPA has invalid length ("
-              <<SubLength<< " bytes). Should be 16 bytes.\n";
+    std::cerr << "Error: Sub record RIDT of REPA has invalid length ("
+              << SubLength << " bytes). Should be 16 bytes.\n";
     return false;
-  }//if
-  //read item data
-  in_File.read((char*) &Weight, 4);
-  in_File.read((char*) &Value, 4);
-  in_File.read((char*) &Uses, 4);
-  in_File.read((char*) &Quality, 4);
+  }
+  // read item data
+  input.read(reinterpret_cast<char*>(&Weight), 4);
+  input.read(reinterpret_cast<char*>(&Value), 4);
+  input.read(reinterpret_cast<char*>(&Uses), 4);
+  input.read(reinterpret_cast<char*>(&Quality), 4);
   BytesRead += 16;
-  if (!in_File.good())
+  if (!input.good())
   {
-    std::cout << "Error while reading subrecord RIDT of REPA.\n";
+    std::cerr << "Error while reading subrecord RIDT of REPA.\n";
     return false;
   }
 
-  //read ITEX
-  in_File.read((char*) &SubRecName, 4);
-  BytesRead += 4;
-  if (SubRecName!=cITEX)
+  // read ITEX
+  if (!loadString256WithHeader(input, InventoryIcon, Buffer, cITEX, BytesRead))
   {
-    UnexpectedRecord(cITEX, SubRecName);
+    std::cerr << "Error while reading subrecord ITEX of REPA.\n";
     return false;
   }
-  //ITEX's length
-  in_File.read((char*) &SubLength, 4);
-  BytesRead += 4;
-  if (SubLength>255)
-  {
-    std::cout << "Error: subrecord FNAM of REPA is longer than 255 characters.\n";
-    return false;
-  }
-  //read icon texture path
-  memset(Buffer, '\0', 256);
-  in_File.read(Buffer, SubLength);
-  BytesRead += SubLength;
-  if (!in_File.good())
-  {
-    std::cout << "Error while reading subrecord ITEX of REPA.\n";
-    return false;
-  }
-  InventoryIcon = std::string(Buffer);
 
-  if (BytesRead<Size)
+  if (BytesRead < Size)
   {
-    //read optional SCRI
-    in_File.read((char*) &SubRecName, 4);
-    if (SubRecName!=cSCRI)
+    // read optional SCRI
+    if (!loadString256WithHeader(input, ScriptName, Buffer, cSCRI, BytesRead))
     {
-      UnexpectedRecord(cSCRI, SubRecName);
-      return false;
-    }
-    //SCRI's length
-    in_File.read((char*) &SubLength, 4);
-    if (SubLength>255)
-    {
-      std::cout << "Error: subrecord SCRI of REPA is longer than 255 characters.\n";
-      return false;
-    }
-    //read script name
-    memset(Buffer, '\0', 256);
-    in_File.read(Buffer, SubLength);
-    if (!in_File.good())
-    {
-      std::cout << "Error while reading subrecord SCRI of REPA.\n";
+      std::cerr << "Error while reading subrecord SCRI of REPA.\n";
       return false;
     }
     ScriptName = std::string(Buffer);
-  }//if
+  }
   else
   {
     ScriptName = "";
   }
-  return in_File.good();
+  return input.good();
 }
 
 bool operator<(const RepairItemRecord& left, const RepairItemRecord& right)
@@ -359,4 +233,4 @@ bool operator<(const RepairItemRecord& left, const RepairItemRecord& right)
   return (lowerCaseCompare(left.recordID, right.recordID)<0);
 }
 
-} //namespace
+} // namespace
--- mw/base/records/RepairItemRecord.hpp
+++ mw/base/records/RepairItemRecord.hpp
@@ -1,7 +1,7 @@
 /*
  -------------------------------------------------------------------------------
     This file is part of the Morrowind Tools Project.
-    Copyright (C) 2011, 2012  Thoronador
+    Copyright (C) 2011, 2012, 2021  Thoronador
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -27,52 +27,57 @@
 namespace MWTP
 {
 
+/** Holds information about an item used to do repairs (e. g. hammer). */
 struct RepairItemRecord: public BasicRecord
 {
-  std::string recordID; //formerly known as RepairItemID
+  std::string recordID;
   std::string ModelPath;
   std::string Name;
-  //repair data
+  // repair data
   float Weight;
   int32_t Value;
   int32_t Uses;
   float Quality;
-  //end of repair data
+  // end of repair data
   std::string InventoryIcon;
   std::string ScriptName;
 
-  /* constructor */
-  RepairItemRecord();
+  /** Creates an empty record with the given ID.
+   *
+   * \param ID  the record's ID
+   */
+  RepairItemRecord(const std::string& ID = "");
 
-  /* alternative constructor */
-  RepairItemRecord(const std::string& ID);
-
-  /* destructor */
-  ~RepairItemRecord();
-
-  /* returns true, if the other record contains the same data */
+  /** \brief Checks whether another instance contains the same data.
+   *
+   * \param other   the other record to compare with
+   * \return Returns true, if @other contains the same data as instance.
+   *         Returns false otherwise.
+   */
   bool equals(const RepairItemRecord& other) const;
 
   #ifndef MW_UNSAVEABLE_RECORDS
-  /* writes the record to the given output stream and returns true on success
-
-    parameters:
-        output - the output stream
-  */
+  /** \brief Writes the record to the given output stream.
+   *
+   * \param output  the output stream
+   * \return Returns true on success (record was written to stream).
+   *         Returns false, if an error occurred.
+   */
   bool saveToStream(std::ostream& output) const override;
   #endif
 
-  /* loads the record from the given input stream and returns true on success
+  /** \brief Loads the record from the given input stream.
+   *
+   * \param input    the input stream
+   * \return Returns true on success (record was loaded from stream).
+   *         Returns false, if an error occurred.
+   */
+  bool loadFromStream(std::istream& input) override;
+}; // struct
 
-    parameters:
-        in_File - the input stream
-  */
-  bool loadFromStream(std::istream& in_File) override;
-};//struct
-
-//comparison operator for ordered set
+// comparison operator for ordered set
 bool operator<(const RepairItemRecord& left, const RepairItemRecord& right);
 
-} //namespace
+} // namespace
 
 #endif // MW_REPAIRITEMRECORD_HPP

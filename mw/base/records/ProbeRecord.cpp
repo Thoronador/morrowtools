70f6ba44aa9000631a1def26f2c79970325d98ec
Dirk Stolle <striezel-dev@web.de>
2021-11-24 03:27:27 +0100

    tests+modernize: MWTP::ProbeRecord

--- mw/base/records/ProbeRecord.cpp
+++ mw/base/records/ProbeRecord.cpp
@@ -1,7 +1,7 @@
 /*
  -------------------------------------------------------------------------------
     This file is part of the Morrowind Tools Project.
-    Copyright (C) 2009, 2011, 2012, 2013  Thoronador
+    Copyright (C) 2009, 2011, 2012, 2013, 2021  Thoronador
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -19,7 +19,6 @@
 */
 
 #include "ProbeRecord.hpp"
-#include <cstring>
 #include <iostream>
 #include "../MW_Constants.hpp"
 #include "../HelperIO.hpp"
@@ -28,69 +27,51 @@
 namespace MWTP
 {
 
-ProbeRecord::ProbeRecord()
+ProbeRecord::ProbeRecord(const std::string& ID)
 : BasicRecord(),
-  recordID(""),
-  ModelPath(""),
-  Name(""),
-  //probe data
+  recordID(ID),
+  ModelPath(std::string()),
+  Name(std::string()),
+  // probe data
   Weight(0.0f),
   Value(0),
   Quality(0.0f),
   Uses(0),
-  //end of probe data
-  InventoryIcon(""),
-  ScriptName("")
-{ }
-
-ProbeRecord::ProbeRecord(const std::string& ID)
+  // end of probe data
+  InventoryIcon(std::string()),
+  ScriptName(std::string())
 {
-  recordID = ID;
-  ModelPath = Name = "";
-  //probe data
-  Weight = 0.0f;
-  Value = 0;
-  Quality = 0.0f;
-  Uses = 0;
-  //end of probe data
-  InventoryIcon = ScriptName = "";
-}
-
-ProbeRecord::~ProbeRecord()
-{
-  //empty
 }
 
 bool ProbeRecord::equals(const ProbeRecord& other) const
 {
-  return ((recordID==other.recordID) and (ModelPath==other.ModelPath)
-      and (Name==other.Name) and (Weight==other.Weight) and (Value==other.Value)
-      and (Quality==other.Quality) and (Uses==other.Uses)
-      and (InventoryIcon==other.InventoryIcon) and (ScriptName==other.ScriptName));
+  return (recordID == other.recordID) && (ModelPath == other.ModelPath)
+      && (Name == other.Name) && (Weight == other.Weight) && (Value == other.Value)
+      && (Quality == other.Quality) && (Uses == other.Uses)
+      && (InventoryIcon == other.InventoryIcon) && (ScriptName == other.ScriptName);
 }
 
 #ifndef MW_UNSAVEABLE_RECORDS
 bool ProbeRecord::saveToStream(std::ostream& output) const
 {
-  output.write((const char*) &cPROB, 4);
-  uint32_t Size;
-  Size = 4 /* NAME */ +4 /* 4 bytes for length */
-        +recordID.length()+1 /* length of ID +1 byte for NUL termination */
-        +4 /* MODL */ +4 /* 4 bytes for length */
-        +ModelPath.length()+1 /* length of path +1 byte for NUL termination */
-        +4 /* FNAM */ +4 /* 4 bytes for length */
-        +Name.length()+1 /* length of name +1 byte for NUL termination */
-        +4 /* PBDT */ +4 /* PBDT's length */ +16 /*size of PBDT (always 16 bytes)*/
-        +4 /* ITEX */ +4 /* 4 bytes for length */
-        +InventoryIcon.length()+1 /* length of icon path +1 byte for NUL termination */;
+  output.write(reinterpret_cast<const char*>(&cPROB), 4);
+  uint32_t Size = 4 /* NAME */ + 4 /* 4 bytes for length */
+        + recordID.length() + 1 /* length of ID +1 byte for NUL termination */
+        + 4 /* MODL */ + 4 /* 4 bytes for length */
+        + ModelPath.length() + 1 /* length of path +1 byte for NUL termination */
+        + 4 /* FNAM */ + 4 /* 4 bytes for length */
+        + Name.length() + 1 /* length of name +1 byte for NUL termination */
+        + 4 /* PBDT */ + 4 /* PBDT's length */ + 16 /* size of PBDT */
+        + 4 /* ITEX */ + 4 /* 4 bytes for length */
+        + InventoryIcon.length() + 1 /* length of path +1 byte for NUL termination */;
   if (!ScriptName.empty())
   {
-    Size = Size +4 /* SCRI */ +4 /* 4 bytes for length */
-          +ScriptName.length()+1; /* length of script ID +1 byte for NUL termination */;
+    Size = Size + 4 /* SCRI */ + 4 /* 4 bytes for length */
+         + ScriptName.length() + 1; /* length of script ID +1 byte for NUL termination */;
   }
-  output.write((const char*) &Size, 4);
-  output.write((const char*) &HeaderOne, 4);
-  output.write((const char*) &HeaderFlags, 4);
+  output.write(reinterpret_cast<const char*>(&Size), 4);
+  output.write(reinterpret_cast<const char*>(&HeaderOne), 4);
+  output.write(reinterpret_cast<const char*>(&HeaderFlags), 4);
 
   /*Probe Item:
     NAME = Item ID, required
@@ -104,71 +85,59 @@ bool ProbeRecord::saveToStream(std::ostream& output) const
     ITEX = Inventory Icon
     SCRI = Script Name (optional) */
 
-  //write NAME
-  output.write((const char*) &cNAME, 4);
-  //NAME's length
-  uint32_t SubLength;
-  SubLength = recordID.length()+1;//length of string plus one for NUL-termination
-  output.write((const char*) &SubLength, 4);
-  //write ID
+  // write ID (NAME)
+  output.write(reinterpret_cast<const char*>(&cNAME), 4);
+  uint32_t SubLength = recordID.length() + 1;
+  output.write(reinterpret_cast<const char*>(&SubLength), 4);
   output.write(recordID.c_str(), SubLength);
 
-  //write MODL
-  output.write((const char*) &cMODL, 4);
-  //MODL's length
-  SubLength = ModelPath.length()+1;//length of string plus one for NUL-termination
-  output.write((const char*) &SubLength, 4);
-  //write model path
+  // write model path (MODL)
+  output.write(reinterpret_cast<const char*>(&cMODL), 4);
+  SubLength = ModelPath.length() + 1;
+  output.write(reinterpret_cast<const char*>(&SubLength), 4);
   output.write(ModelPath.c_str(), SubLength);
 
-  //write FNAM
-  output.write((const char*) &cFNAM, 4);
-  //FNAM's length
-  SubLength = Name.length()+1;//length of string plus one for NUL-termination
-  output.write((const char*) &SubLength, 4);
-  //write probe name
+  // write probe name (FNAM)
+  output.write(reinterpret_cast<const char*>(&cFNAM), 4);
+  SubLength = Name.length() + 1;
+  output.write(reinterpret_cast<const char*>(&SubLength), 4);
   output.write(Name.c_str(), SubLength);
 
-  //write PBDT
-  output.write((const char*) &cPBDT, 4);
-  //PBDT's length
-  SubLength = 16;//length of PBDT is always 16 bytes
-  output.write((const char*) &SubLength, 4);
-  //write repair item data
-  output.write((const char*) &Weight, 4);
-  output.write((const char*) &Value, 4);
-  output.write((const char*) &Quality, 4);
-  output.write((const char*) &Uses, 4);
+  // write probe data (PBDT)
+  output.write(reinterpret_cast<const char*>(&cPBDT), 4);
+  SubLength = 16;
+  output.write(reinterpret_cast<const char*>(&SubLength), 4);
+  // write probe data
+  output.write(reinterpret_cast<const char*>(&Weight), 4);
+  output.write(reinterpret_cast<const char*>(&Value), 4);
+  output.write(reinterpret_cast<const char*>(&Quality), 4);
+  output.write(reinterpret_cast<const char*>(&Uses), 4);
 
-  //write ITEX
-  output.write((const char*) &cITEX, 4);
-  //ITEX's length
-  SubLength = InventoryIcon.length()+1;//length of string plus one for NUL-termination
-  output.write((const char*) &SubLength, 4);
-  //write inventory icon path
+  // write inventory icon path (ITEX)
+  output.write(reinterpret_cast<const char*>(&cITEX), 4);
+  SubLength = InventoryIcon.length() + 1;
+  output.write(reinterpret_cast<const char*>(&SubLength), 4);
   output.write(InventoryIcon.c_str(), SubLength);
 
   if (!ScriptName.empty())
   {
-    //write SCRI
-    output.write((const char*) &cSCRI, 4);
-    //SCRI's length
-    SubLength = ScriptName.length()+1;//length of string plus one for NUL-termination
-    output.write((const char*) &SubLength, 4);
-    //write script name/ID
+    // write script name / ID (SCRI)
+    output.write(reinterpret_cast<const char*>(&cSCRI), 4);
+    SubLength = ScriptName.length() + 1;
+    output.write(reinterpret_cast<const char*>(&SubLength), 4);
     output.write(ScriptName.c_str(), SubLength);
-  }//if
+  }
 
   return output.good();
 }
 #endif
 
-bool ProbeRecord::loadFromStream(std::istream& in_File)
+bool ProbeRecord::loadFromStream(std::istream& input)
 {
-  uint32_t Size;
-  in_File.read((char*) &Size, 4);
-  in_File.read((char*) &HeaderOne, 4);
-  in_File.read((char*) &HeaderFlags, 4);
+  uint32_t Size = 0;
+  input.read((char*) &Size, 4);
+  input.read((char*) &HeaderOne, 4);
+  input.read((char*) &HeaderFlags, 4);
 
   /*Probe Item:
     NAME = Item ID, required
@@ -182,184 +151,87 @@ bool ProbeRecord::loadFromStream(std::istream& in_File)
     ITEX = Inventory Icon
     SCRI = Script Name (optional) */
 
-  uint32_t SubRecName;
-  uint32_t SubLength, BytesRead;
-  SubRecName = SubLength = 0;
+  uint32_t SubRecName = 0;
+  uint32_t SubLength = 0;
+  uint32_t BytesRead = 0;
 
-  //read NAME
-  in_File.read((char*) &SubRecName, 4);
-  BytesRead = 4;
-  if (SubRecName!=cNAME)
-  {
-    UnexpectedRecord(cNAME, SubRecName);
-    return false;
-  }
-  //NAME's length
-  in_File.read((char*) &SubLength, 4);
-  BytesRead += 4;
-  if (SubLength>255)
-  {
-    std::cout << "Error: subrecord NAME of PROB is longer than 255 characters.\n";
-    return false;
-  }
-  //read repair item ID
+  // read probe ID (NAME)
   char Buffer[256];
-  memset(Buffer, '\0', 256);
-  in_File.read(Buffer, SubLength);
-  BytesRead += SubLength;
-  if (!in_File.good())
+  if (!loadString256WithHeader(input, recordID, Buffer, cNAME, BytesRead))
   {
-    std::cout << "Error while reading subrecord NAME of PROB.\n";
+    std::cerr << "Error while reading subrecord NAME of PROB.\n";
     return false;
   }
-  recordID = std::string(Buffer);
 
-  //read MODL
-  in_File.read((char*) &SubRecName, 4);
-  BytesRead += 4;
-  if (SubRecName!=cMODL)
+  // read model path (MODL)
+  if (!loadString256WithHeader(input, ModelPath, Buffer, cMODL, BytesRead))
   {
-    UnexpectedRecord(cMODL, SubRecName);
+    std::cerr << "Error while reading subrecord MODL of PROB.\n";
     return false;
   }
-  //MODL's length
-  in_File.read((char*) &SubLength, 4);
-  BytesRead += 4;
-  if (SubLength>255)
-  {
-    std::cout << "Error: subrecord MODL of PROB is longer than 255 characters.\n";
-    return false;
-  }
-  //read model path
-  memset(Buffer, '\0', 256);
-  in_File.read(Buffer, SubLength);
-  BytesRead += SubLength;
-  if (!in_File.good())
-  {
-    std::cout << "Error while reading subrecord MODL of PROB.\n";
-    return false;
-  }
-  ModelPath = std::string(Buffer);
 
-  //read FNAM
-  in_File.read((char*) &SubRecName, 4);
-  BytesRead += 4;
-  if (SubRecName!=cFNAM)
+  // read FNAM
+  if (!loadString256WithHeader(input, Name, Buffer, cFNAM, BytesRead))
   {
-    UnexpectedRecord(cFNAM, SubRecName);
+    std::cerr << "Error while reading subrecord FNAM of PROB.\n";
     return false;
   }
-  //FNAM's length
-  in_File.read((char*) &SubLength, 4);
-  BytesRead += 4;
-  if (SubLength>255)
-  {
-    std::cout << "Error: subrecord FNAM of PROB is longer than 255 characters.\n";
-    return false;
-  }
-  //read name
-  memset(Buffer, '\0', 256);
-  in_File.read(Buffer, SubLength);
-  BytesRead += SubLength;
-  if (!in_File.good())
-  {
-    std::cout << "Error while reading subrecord FNAM of PROB.\n";
-    return false;
-  }
-  Name = std::string(Buffer);
 
-  //read PBDT
-  in_File.read((char*) &SubRecName, 4);
+  // read PBDT
+  input.read(reinterpret_cast<char*>(&SubRecName), 4);
   BytesRead += 4;
-  if (SubRecName!=cPBDT)
+  if (SubRecName != cPBDT)
   {
     UnexpectedRecord(cPBDT, SubRecName);
     return false;
   }
-  //PBDT's length
-  in_File.read((char*) &SubLength, 4);
+  // PBDT's length
+  input.read(reinterpret_cast<char*>(&SubLength), 4);
   BytesRead += 4;
-  if (SubLength!=16)
+  if (SubLength != 16)
   {
-    std::cout << "Error: sub record PBDT of REPA has invalid length ("
-              <<SubLength<< " bytes). Should be 16 bytes.\n";
+    std::cerr << "Error: Sub record PBDT of PROB has invalid length ("
+              << SubLength << " bytes). Should be 16 bytes.\n";
     return false;
-  }//if
-  //read item data
-  in_File.read((char*) &Weight, 4);
-  in_File.read((char*) &Value, 4);
-  in_File.read((char*) &Quality, 4);
-  in_File.read((char*) &Uses, 4);
+  }
+  // read item data
+  input.read(reinterpret_cast<char*>(&Weight), 4);
+  input.read(reinterpret_cast<char*>(&Value), 4);
+  input.read(reinterpret_cast<char*>(&Quality), 4);
+  input.read(reinterpret_cast<char*>(&Uses), 4);
   BytesRead += 16;
-  if (!in_File.good())
+  if (!input.good())
   {
-    std::cout << "Error while reading subrecord PBDT of REPA.\n";
+    std::cerr << "Error while reading subrecord PBDT of PROB.\n";
     return false;
   }
 
-  //read ITEX
-  in_File.read((char*) &SubRecName, 4);
-  BytesRead += 4;
-  if (SubRecName!=cITEX)
+  // read icon texture path (ITEX)
+  if (!loadString256WithHeader(input, InventoryIcon, Buffer, cITEX, BytesRead))
   {
-    UnexpectedRecord(cITEX, SubRecName);
+    std::cerr << "Error while reading subrecord ITEX of PROB.\n";
     return false;
   }
-  //ITEX's length
-  in_File.read((char*) &SubLength, 4);
-  BytesRead += 4;
-  if (SubLength>255)
-  {
-    std::cout << "Error: subrecord FNAM of PROB is longer than 255 characters.\n";
-    return false;
-  }
-  //read icon texture path
-  memset(Buffer, '\0', 256);
-  in_File.read(Buffer, SubLength);
-  BytesRead += SubLength;
-  if (!in_File.good())
-  {
-    std::cout << "Error while reading subrecord ITEX of PROB.\n";
-    return false;
-  }
-  InventoryIcon = std::string(Buffer);
 
-  if (BytesRead<Size)
+  if (BytesRead < Size)
   {
-    //read optional SCRI
-    in_File.read((char*) &SubRecName, 4);
-    if (SubRecName!=cSCRI)
+    // read optional script name (SCRI)
+    if (!loadString256WithHeader(input, ScriptName, Buffer, cSCRI, BytesRead))
     {
-      UnexpectedRecord(cSCRI, SubRecName);
+      std::cerr << "Error while reading subrecord SCRI of PROB.\n";
       return false;
     }
-    //SCRI's length
-    in_File.read((char*) &SubLength, 4);
-    if (SubLength>255)
-    {
-      std::cout << "Error: subrecord SCRI of PROB is longer than 255 characters.\n";
-      return false;
-    }
-    //read script name
-    memset(Buffer, '\0', 256);
-    in_File.read(Buffer, SubLength);
-    if (!in_File.good())
-    {
-      std::cout << "Error while reading subrecord SCRI of PROB.\n";
-      return false;
-    }
-    ScriptName = std::string(Buffer);
-  }//if
+  }
   else
   {
     ScriptName = "";
   }
-  return in_File.good();
+  return true;
 }
 
 bool operator<(const ProbeRecord& left, const ProbeRecord& right)
 {
-  return (lowerCaseCompare(left.recordID, right.recordID)<0);
+  return lowerCaseCompare(left.recordID, right.recordID) < 0;
 }
 
-} //namespace
+} // namespace
--- mw/base/records/ProbeRecord.hpp
+++ mw/base/records/ProbeRecord.hpp
@@ -1,7 +1,7 @@
 /*
  -------------------------------------------------------------------------------
     This file is part of the Morrowind Tools Project.
-    Copyright (C) 2011, 2012  Thoronador
+    Copyright (C) 2011, 2012, 2021  Thoronador
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -27,52 +27,57 @@
 namespace MWTP
 {
 
+/** Holds data of a probe. */
 struct ProbeRecord: public BasicRecord
 {
-  std::string recordID; //formerly ProbeID
+  std::string recordID;
   std::string ModelPath;
   std::string Name;
-  //probe data
+  // probe data
   float Weight;
   int32_t Value;
   float Quality;
   int32_t Uses;
-  //end of probe data
+  // end of probe data
   std::string InventoryIcon;
   std::string ScriptName;
 
-  /* constructor */
-  ProbeRecord();
+  /** Creates an empty probe with the given ID.
+   *
+   * \param ID  the ID of the probe
+   */
+  ProbeRecord(const std::string& ID = "");
 
-  /* alternative constructor */
-  ProbeRecord(const std::string& ID);
-
-  /* destructor */
-  ~ProbeRecord();
-
-  /* returns true, if the other record contains the same data */
+  /** \brief Checks whether another instance contains the same data.
+   *
+   * \param other   the other record to compare with
+   * \return Returns true, if @other contains the same data as instance.
+   *         Returns false otherwise.
+   */
   bool equals(const ProbeRecord& other) const;
 
   #ifndef MW_UNSAVEABLE_RECORDS
-  /* writes the record to the given output stream and returns true on success
-
-    parameters:
-        output - the output stream
-  */
+  /** \brief Writes the record to the given output stream.
+   *
+   * \param output  the output stream
+   * \return Returns true on success (record was written to stream).
+   *         Returns false, if an error occurred.
+   */
   bool saveToStream(std::ostream& output) const override;
   #endif
 
-  /* loads the record from the given input stream and returns true on success
+  /** \brief Loads the record from the given input stream.
+   *
+   * \param input    the input stream
+   * \return Returns true on success (record was loaded from stream).
+   *         Returns false, if an error occurred.
+   */
+  bool loadFromStream(std::istream& input) override;
+}; // struct
 
-    parameters:
-        in_File - the input stream
-  */
-  bool loadFromStream(std::istream& in_File) override;
-};//struct
-
-//comparison operator for ordered set
+// comparison operator for ordered set
 bool operator<(const ProbeRecord& left, const ProbeRecord& right);
 
-} //namespace
+} // namespace
 
 #endif // MW_PROBERECORD_HPP

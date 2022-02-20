a1ea4c26bccd87b2ef1924d9ccf5a1a7d26f4b64
Dirk Stolle <striezel-dev@web.de>
2022-02-20 19:58:33 +0100

    tests+modernize: MWTP::NPCRecord::equals()

--- tests/components/mw/base/records/NPCRecord.cpp
+++ tests/components/mw/base/records/NPCRecord.cpp
@@ -74,6 +74,297 @@ TEST_CASE("MWTP::NPCRecord")
     REQUIRE( record.NPC_Flag == 0 );
   }
 
+  SECTION("equals")
+  {
+    NPCRecord a;
+    NPCRecord b;
+
+    SECTION("equal")
+    {
+      a.recordID = "NPC_One";
+      a.Name = "foobar";
+
+      b.recordID = "NPC_One";
+      b.Name = "foobar";
+
+      REQUIRE( a.equals(b) );
+      REQUIRE( b.equals(a) );
+    }
+
+    SECTION("unequal")
+    {
+      SECTION("recordID mismatch")
+      {
+        a.recordID = "foo";
+        b.recordID = "bar";
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("Name mismatch")
+      {
+        a.Name = "One";
+        b.Name = "Another";
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("ModelPath mismatch")
+      {
+        a.ModelPath = "foo.nif";
+        b.ModelPath = "bar.nif";
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("RaceID mismatch")
+      {
+        a.RaceID = "foo";
+        b.RaceID = "bar";
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("FactionID mismatch")
+      {
+        a.FactionID = "foo";
+        b.FactionID = "bar";
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("HeadModel mismatch")
+      {
+        a.HeadModel = "head.nif";
+        b.HeadModel = "box.nif";
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("ClassID mismatch")
+      {
+        a.ClassID = "foo";
+        b.ClassID = "bar";
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("HairModel mismatch")
+      {
+        a.HairModel = "hair.nif";
+        b.HairModel = "whatever.nif";
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("ScriptID mismatch")
+      {
+        a.ScriptID = "foo";
+        b.ScriptID = "bar";
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("Level mismatch")
+      {
+        a.Level = 15;
+        b.Level = 25;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("Strength mismatch")
+      {
+        a.Strength = 15;
+        b.Strength = 25;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("Intelligence mismatch")
+      {
+        a.Intelligence = 15;
+        b.Intelligence = 25;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("Willpower mismatch")
+      {
+        a.Willpower = 15;
+        b.Willpower = 25;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("Agility mismatch")
+      {
+        a.Agility = 15;
+        b.Agility = 25;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("Speed mismatch")
+      {
+        a.Speed = 15;
+        b.Speed = 25;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("Endurance mismatch")
+      {
+        a.Endurance = 15;
+        b.Endurance = 25;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("Personality mismatch")
+      {
+        a.Personality = 15;
+        b.Personality = 25;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("Luck mismatch")
+      {
+        a.Luck = 15;
+        b.Luck = 25;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("Skills mismatch")
+      {
+        a.Skills[15] = 15;
+        b.Skills[15] = 25;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("Reputation mismatch")
+      {
+        a.Reputation = 60;
+        b.Reputation = 90;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("Health mismatch")
+      {
+        a.Health = 15;
+        b.Health = 25;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("SpellPoints mismatch")
+      {
+        a.SpellPoints = 15;
+        b.SpellPoints = 25;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("Fatigue mismatch")
+      {
+        a.Fatigue = 15;
+        b.Fatigue = 25;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("Disposition mismatch")
+      {
+        a.Disposition = 0;
+        b.Disposition = 25;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("Data_FactionID mismatch")
+      {
+        a.Data_FactionID = 15;
+        b.Data_FactionID = 25;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("Rank mismatch")
+      {
+        a.Rank = 15;
+        b.Rank = 25;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("Unknown1 mismatch")
+      {
+        a.Unknown1 = 15;
+        b.Unknown1 = 25;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("Gold mismatch")
+      {
+        a.Gold = 15;
+        b.Gold = 25;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("NPCDataType mismatch")
+      {
+        a.NPCDataType = ndt12Bytes;
+        b.NPCDataType = ndt52Bytes;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+
+      SECTION("NPC_Flag mismatch")
+      {
+        a.NPC_Flag = 0x000000FF;
+        b.NPC_Flag = 0x000000AB;
+
+        REQUIRE_FALSE( a.equals(b) );
+        REQUIRE_FALSE( b.equals(a) );
+      }
+    }
+  }
+
   SECTION("fun with flags")
   {
     NPCRecord record;

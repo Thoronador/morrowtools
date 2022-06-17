96d8479a63c7b293faa778435bd66da896473d7c
Dirk Stolle <striezel-dev@web.de>
2022-06-17 17:22:17 +0200

    use pkg-config to find liblz4 dependency

--- cmake/lz4-config.cmake
+++ cmake/lz4-config.cmake
@@ -5,7 +5,11 @@
 #  LZ4_INCLUDE_DIRS - The liblz4 include directories
 #  LZ4_LIBRARIES - The libraries needed to use liblz4
 
-find_path(LZ4_INCLUDE_PATH lz4.h /usr/include/)
+find_package(PkgConfig)
+pkg_check_modules(PC_LIBLZ4 liblz4)
+
+find_path(LZ4_INCLUDE_PATH lz4.h
+          HINTS /usr/include/ /mingw64/include/ ${PC_LIBLZ4_INCLUDE_DIRS})
 if (LZ4_INCLUDE_PATH)
   set(LZ4_INCLUDE_DIRS
     ${LZ4_INCLUDE_PATH}
@@ -14,7 +18,8 @@ else ()
   message("Include path for liblz4 could not be determined!")
 endif (LZ4_INCLUDE_PATH)
 
-find_library(LZ4_LIBRARIES lz4)
+find_library(LZ4_LIBRARIES NAMES lz4 liblz4
+             HINTS ${PC_LIBLZ4_LIBRARY_DIRS})
 
 if (NOT LZ4_LIBRARIES)
    message( "Library for lz4 was not found!")

ed3a0e15fb359b14eefa0924a5403abce5bf5c3d
Dirk Stolle <striezel-dev@web.de>
2024-12-06 18:53:19 +0100

    formID_finder: replace outdated WinAPI function call
    The SetWindowLong function has been superseded by the newer and
    more portable SetWindowLongPtr function. SetWindowLongPtr should
    work on both 32-bit and 64-bit Windows OSes, according to its
    documentation.

--- apps/sr/formID_finder_GUI/formid_finder.pas
+++ apps/sr/formID_finder_GUI/formid_finder.pas
@@ -1,7 +1,7 @@
 {
  -------------------------------------------------------------------------------
     This file is part of the Skyrim Tools Project.
-    Copyright (C) 2012, 2013  Thoronador
+    Copyright (C) 2012, 2013, 2024  Dirk Stolle
 
     This programme is free software: you can redistribute it and/or
     modify it under the terms of the GNU General Public License as published
@@ -593,7 +593,7 @@ begin
   m_HasData := false;
   m_StringData := '';
   {$IFDEF WINDOWS }
-  m_PrevWndProc := Windows.WNDPROC(SetWindowLong(Self.Handle, GWL_WNDPROC, PtrInt(@WndCallback)));
+  m_PrevWndProc := Windows.WNDPROC(SetWindowLongPtr(Self.Handle, GWL_WNDPROC, PtrInt(@WndCallback)));
   {$ENDIF}
   Application.Title:= self.Caption;
 end;
